2022b8476a83278b7f9b9de864773e568203a31a
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-08-08 15:58:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item {
+    int value;
+    int weight;
+};
+
+
+// } Driver Code Ends
+// class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+class Solution {
+  public:
+    // Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int w, Item arr[], int n) {
+        // Your code here
+        // first we have to arrange iin value/weight order
+        
+        priority_queue<pair<double,int>> q;
+        for(int i=0;i<n;i++){
+            q.push({(double)arr[i].value/arr[i].weight , i});
+        }
+        
+        double ans=0;
+        
+        while(!q.empty() && w>0){
+            auto i = q.top();q.pop();
+            if(arr[i.second].weight <=w ){
+                ans += arr[i.second].value;
+                w -= arr[i.second].weight;
+            }
+            else{
+                ans += (double)arr[i.second].value*w / arr[i.second].weight;
+                break;
+            }
+            
+        }
+        
+        return ans;
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    // taking testcases
+    cin >> t;
+    cout << setprecision(6) << fixed;
+    while (t--) {
+        // size of array and weight
+        int n, W;
+        cin >> n >> W;
+
+        Item arr[n];
+        // value and weight of each item
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i].value >> arr[i].weight;
+        }
+
+        // function call
+        Solution ob;
+        cout << ob.fractionalKnapsack(W, arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
