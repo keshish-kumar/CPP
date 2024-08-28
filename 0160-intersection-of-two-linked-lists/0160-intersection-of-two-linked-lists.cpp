df7c4feabd9b688f555d3d6a81b580c4ea55fa92
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-08-28 22:47:44 +0530

    Time: 31 ms (90.99%), Space: 17.4 MB (58.94%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,21 +9,56 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> st;
-        ListNode* temp=headA;
-        while(temp!=NULL){
-            st.insert(temp);
-            temp=temp->next;
+        // unordered_set<ListNode*> st;
+        // ListNode* temp=headA;
+        // while(temp!=NULL){
+        //     st.insert(temp);
+        //     temp=temp->next;
+        // }
+
+        // ListNode* temp2=headB;
+        // while(temp2!=NULL){
+        //     if(st.find(temp2)!=st.end()){
+        //             break;
+        //     }
+        //     temp2=temp2->next;
+        // }
+
+        // return temp2;
+
+        int count1=0;
+        int count2=0;
+        ListNode* temp1=headA;
+        while(temp1!=NULL){
+            count1++;
+            temp1=temp1->next;
         }
 
         ListNode* temp2=headB;
         while(temp2!=NULL){
-            if(st.find(temp2)!=st.end()){
-                    break;
-            }
+            count2++;
             temp2=temp2->next;
         }
+        temp1=headA;
+        temp2=headB;
+        if(count1>count2){
+            while(count1>count2){
+                count1--;
+                temp1=temp1->next;
+            }
+        }
+        else if(count1<count2){
+            while(count1<count2){
+                count2--;
+                temp2=temp2->next;
+            }
+        }
 
-        return temp2;
+        while(temp1!=NULL && temp2!=NULL){
+            if(temp1==temp2) return temp1;
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        return NULL;
     }
 };
