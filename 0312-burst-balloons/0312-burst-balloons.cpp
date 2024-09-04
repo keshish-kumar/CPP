6f93203985e873a86dd567f969c1d23a00caaa79
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-04 18:07:20 +0530

    Time: 100 ms (93%), Space: 12.3 MB (32.29%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -16,8 +16,22 @@ public:
 
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return solve(nums,1,nums.size()-2,dp);
+        // vector<vector<int>> dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        // return solve(nums,1,nums.size()-2,dp);
 
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),0));
+
+        for(int i = nums.size()-2;i>=1;i--){
+            for(int j=1;j<=nums.size()-2;j++){
+                if(i>j) continue;
+                int maxi = INT_MIN;
+                for(int index = i;index<=j;index++){
+                    int temp = dp[i][index-1]+dp[index+1][j]+(nums[i-1]*nums[index]*nums[j+1]);
+                maxi = max(maxi,temp);
+                 }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][nums.size()-2];
     }
 };
