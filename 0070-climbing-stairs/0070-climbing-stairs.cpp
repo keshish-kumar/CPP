7bc29725176fc6644c24654cb318b8981f8bbe4e
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-08 15:26:13 +0530

    Time: 0 ms (100%), Space: 7.5 MB (48.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,7 +7,13 @@ public:
         return dp[i] = solve(n,i+1,dp)+solve(n,i+2,dp);
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,0,dp);
+        vector<int> dp(n+1,0);
+        //return solve(n,0,dp);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
