e3d50c1cfb6d63aaee3db1db1784dd76699c1605
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-08 17:42:24 +0530

    Time: 0 ms (100%), Space: 9.5 MB (62.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,18 +3,20 @@ public:
 
     int house(vector<int>& nums, int i,vector<int>& dp){
         if(i>=nums.size()) return 0;
-        int maxi = 0;
+        
         if(dp[i]!=-1) return dp[i];
-        for(int k=i;k<nums.size();k++){
-            int temp = nums[k]+house(nums,k+2,dp);
-            maxi = max(maxi,temp);
-        }
-        return dp[i] = maxi;
+       int steal = nums[i]+ house(nums,i+2,dp);
+       int skip = house(nums,i+1,dp);
+        return dp[i] = max(steal,skip);
     }    
 
     int rob(vector<int>& nums) {
         // House robber
         vector<int> dp(nums.size()+1,-1);
         return house(nums,0,dp);
+
+
+
+        
     }
 };
