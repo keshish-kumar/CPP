baf45c1e77cc92756bd78bd09e6b8343369433b8
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-10 11:31:39 +0530

    Time: 1955 ms (5%), Space: 504.9 MB (5%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            int target = -1*nums[i];
+            unordered_set<int> mp;
+            int left = i+1,right=nums.size()-1;
+            while(left<right){
+                if((nums[left]+nums[right])==target){
+                    ans.insert({-1*target,nums[left],nums[right]});
+                    left++;
+                }
+                else if((nums[left]+nums[right]>target)){
+                    right--;
+                }
+                else
+                    left++;
+            }
+        }
+        
+        vector<vector<int>> a;
+        for(auto it:ans)
+            a.push_back(it);
+        return a;
+    }
+};
