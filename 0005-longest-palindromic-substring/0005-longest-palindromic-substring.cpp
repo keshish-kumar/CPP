dbc449a3da4d8cae5f9cc464b9007e970db47ebc
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-16 21:09:34 +0530

    Time: 320 ms (21.88%), Space: 290.9 MB (12.7%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool solve(string& s, int i, int j){
+    bool solve(string& s, int i, int j,vector<vector<int>>& dp){
         if(i>=j) return true;
-        //if(dp[i][j]!=-1) return dp[i][j] ; vector<vector<int>>& dp
-        if(s[i]==s[j])
-            return solve(s,i+1,j-1);
-        else return  false;
+        if(dp[i][j]!=-1) return dp[i][j]; 
+        if(s[i] == s[j])
+            return dp[i][j] = solve(s,i+1,j-1,dp);
+        else return dp[i][j] = false;
     }
     string longestPalindrome(string s) {
         
         int maxi = INT_MIN;
         int start = 0;
-       // vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,-1));
+        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1,-1));
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
-                if( solve(s,i,j) == true ){
+                if( solve(s,i,j,dp) == true ){
                     if((j-i+1)>maxi){
                         start = i;
                         maxi = (j-i+1);
