a0509b6605f072338b98b5ba57caaf52bdba6d9d
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-17 12:38:16 +0530

    Time: 29 ms (16.98%), Space: 16.4 MB (15.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    
     int lengthOfLongestSubstring(string s) {
-        // here we will use sliding window protocol
-        int i=0,j=0;
-        int maxi = 0;
         set<char> st;
+        int i=0,j=0;
+        int ans =0;
         while(j<s.size()){
-            if(st.find(s[j]) == st.end()){
-                maxi = max(j-i+1,maxi);
-                st.insert(s[j]);
-                j++;
-            }
-            else{
-                
+            while(i<=j && st.find(s[j])!=st.end()){
                 st.erase(s[i]);
                 i++;
             }
+            st.insert(s[j]);
+
+            ans = max(ans,(j-i+1));
+            
+            j++;
         }
-        return maxi;
+        return ans;
     }
 };
