d9854ea26ec95527cffa2486bdd174b1e594a44b
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-19 13:04:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.cpp
@@ -8,34 +8,35 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-	int search(string pat, string txt) {
+	int search(string txt, string pat) {
 	    // code here
-	    map<char,int> mp1,mp2;
+	   map<char, int> mp;
 	    int ans=0;
-	    for(auto it:pat)
-	            mp2[it]++;
-	   int j=0,i=0;
-	   while(j<txt.size()){
-	       
+	    for(int i=0;i<txt.size();i++){
+	        mp[txt[i]]++;
+	    }
+	    int count = mp.size();
+	    int i=0,j=0;
+	
+	    while(j<pat.size()){
+	       // cout<<i<<" "<<j<<endl;
+	        // first we havce to check wethher that elemet is present in map or not
+	        if(mp.find(pat[j])!=mp.end()){
+	            mp[pat[j]]--;
+	            if(mp[pat[j]]==0) count--;
+	        }
+	        
+	        // if the size is equals to window size
+	        if((j-i+1)==txt.size()){
+              
+	            if(count==0) ans++;
+	            if(mp.find(pat[i])!=mp.end()){  if(mp[pat[i]]==0)count++; mp[pat[i]]++;}
+	            i++;
+	        }
+	        j++;
 	      
-	           
-	       mp1[txt[j]]++;
-	           
-	       
-	       if((j-i+1)==pat.size()){
-	           
-	           if(mp1==mp2) ans++;
-	           
-	           mp1[txt[i]]--;
-	           if(mp1[txt[i]]==0) mp1.erase(txt[i]);
-	           
-	           i++;
-	       }
-	       j++;
-	       
-	   }
-	   
-	   return ans;
+	    }
+	    return ans;
 	}
 
 };
