b5d67b3ab0b90f938f374ea6d3fdc86d95fe546a
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-21 12:55:05 +0530

    Time: 9 ms (12.99%), Space: 7.7 MB (75.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
-    int solve(int i, int j, int m , int n, int & ans,vector<vector<int>>& dp){
-        if(i==m && j==n){
-            
+    int solve(int m, int n, int i, int j,vector<vector<int>>& dp){
+        if(i==m-1 && j==n-1){
             return 1;
         }
+        
         if(dp[i][j]!=-1) return dp[i][j];
-        int up=0;
-        if(i<m){
-            up = solve(i+1,j,m,n,ans,dp);
-        }
+        
         int down=0;
-        if(j<n){
-            down = solve(i,j+1,m,n,ans,dp);
-        }
-        return dp[i][j] = up+down;
+        if(i+1<m )
+         down =  solve(m,n,i+1,j,dp);
+        int up=0;
+        if(j+1 < n)
+           up = solve(m,n,i,j+1,dp);
+        
+        return dp[i][j] = down+up;
         
     }
+    
     int uniquePaths(int m, int n) {
-        int ans=0;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m-1,n-1,ans,dp);
         
+        
+        
+        vector<int> dp(n+1,0);
+      
+        for(int i=0;i<m;i++){
+             vector<int> temp(n+1,0);
+            for(int j=0;j<n;j++){
+                
+              if(i==0 && j==0) temp[j]=1;
+               else{ int down=0;
+                 if((i-1) >= 0 )
+                    down =  dp[j];
+                int up=0;
+                if((j-1) >= 0)
+                    up = temp[j-1];
+            temp[j] = up+down;}
+               cout<<dp[j]<<" ";
+            }
+            dp = temp;
+            cout<<endl;
+        }
+       
+        return dp[n-1];
     }
 };
