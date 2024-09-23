d7ecc1473b48136d8104177af512cca3eaa41a70
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-23 21:27:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.cpp
@@ -12,24 +12,14 @@ class Solution {
         // fist we have to take sum of numbers of elemnt which will have repeating number
         int n = arr.size();
         long long n1 = 0;
-        
-        for(int i=0;i<n;i++){
-            n1 += arr[i]-(i+1);
-        }
-        
-        
-        // Now we have to get sum of squares of the numbers.
         long long n2=0;
         for(int i=0;i<n;i++){
+            n1 += arr[i]-(i+1);
             n2 += pow(arr[i],2)-pow((i+1),2);
         }
-        
         n2= n2/n1;
-        //cout<<n1<<" "<<n2<<endl;
-        // x1-y1=n1 && x2+y2=n2;
         long long x1 = (n1+n2)/2; // repeating number 
         long long y1 = n2-x1; // missing number
-        
         return {x1,y1};
         
     }
