0d168a623d64133cdb70943840b0f211c381fdcb
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-23 12:25:19 +0530

    Time: 67 ms (10.82%), Space: 51.3 MB (16.75%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,11 +21,32 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         // here we have infinit supply of coins.
-        int ans = INT_MAX;
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
-        ans =  solve(coins,amount,0,dp);
+        // int ans = INT_MAX;
+         vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,INT_MAX));
+        // ans =  solve(coins,amount,0,dp);
       
-        if(ans==INT_MAX) return -1; else return ans;
-        
+        // if(ans==INT_MAX) return -1; else return ans;
+
+        // Now we will do tabulation approach
+        for(int i=0;i<=coins.size();i++){ // i I will consider it as index of coins
+            for(int j=0;j<=amount;j++){   // j I will consider it as amount
+                if(i==0 && j==0){dp[i][j]=0;}
+                else if(i==0) {dp[i][j]=INT_MAX;}
+                else if(j==0){dp[i][j]=0;}
+                else{
+                     int take=INT_MAX;
+                    if((coins[i-1]) <= j ){
+                            int temptake =  dp[i][j-coins[i-1]];
+                            if(temptake!=INT_MAX) take = temptake+1;
+    
+                        }
+                        // not pick
+                    int nottake =  dp[i-1][j];
+                    dp[i][j] = min(take,nottake);
+                }
+            }
+        }
+     if(dp[coins.size()][amount]==INT_MAX) return -1;
+     else return dp[coins.size()][amount];
     }
 };
