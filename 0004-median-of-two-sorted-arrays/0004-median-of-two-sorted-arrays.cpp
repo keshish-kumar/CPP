d5896f605798461c21d493286c53d1d73be1966b
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-29 18:10:12 +0530

    Time: 23 ms (68.29%), Space: 94.6 MB (87.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size()+nums2.size();
-       
-         int   index1=n/2;
-          int   index2=index1-1;
-        
-        int i=0,j=0;
-        int n1=0,n2=0;
-        int count=0;
+        // Now we will aply binary search to it. 
+        // we need to check which is samller size and apply binary search to samller vector
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n = nums1.size();
+        int m = nums2.size();
+        // Now apply low to 0 an high to nums1.size()-1;
+        int low = 0;
+        int high = n;
+        while(low<=high){
+            int px = (low+high)/2; // element which will be from nums1 in left slot
+            int py = (n+m+1)/2-px; // elemnet which qill be from nums2 in left slot left after filling from nums1
+            
+            // first half 
+            int x1 = (px==0)?-1e9: nums1[px-1];
+            int x2 = (py==0)?-1e9: nums2[py-1];
 
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                if(count==index1) n1 = nums1[i];
-                if(count==index2) n2 = nums1[i];
-                i++;
-            }
-            else{
-                if(count==index1) n1 = nums2[j]; 
-                if(count==index2) n2 = nums2[j];
-                j++;
-            }
-            count++;
-        }
-        while(i<nums1.size()){
-            if(count==index1) n1 = nums1[i];
-                if(count==index2) n2 = nums1[i];
-                i++;
-            count++;
-        }
-        while(j<nums2.size()){
-            if(count==index1) n1 = nums2[j];
-                if(count==index2) n2 = nums2[j];
-                j++;
-            count++;
-        }
+            // second half
+            int x3 = (px==n)?1e9: nums1[px];
+            int x4 = (py==m)?1e9: nums2[py];
 
-        if(n%2!=0) return n1;
-        else return double(n1+n2)/2;
+            if(x1<=x4 && x2<=x3){
+                if((n+m)%2==0) return (max(x1,x2)+min(x3,x4))/2.0;
+                else return max(x1,x2);
+            }
+            if(x1>x4) high = px-1;
+            else low = px+1;
+        }
+        return 0;
+
     }
 };
