1d1fde0d8ca5b64f6b1005a4f864252126efbc32
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-30 19:46:57 +0530

    Time: 0 ms (100%), Space: 8.2 MB (73.56%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long nn = n; // here we are taking lpong lpong beacuse int might exceede its capacity
-        if(nn<0) nn = -1*nn;  // this is to handle any neagtive numbners 
-        double ans = 1; // when 2^10 is converted to power 5 (10/2) than ans is used to store that odd value 4*4^4                          // which is 4*ans; 
-        while(nn>0){
-            
-            if(nn%2==1){
-                ans = ans*x;
-                nn = nn-1;
+    double myPow(double x, int nn) {
+        double answer  = 1;
+        long long n = nn;
+        if(n<0) n=-1*n;
+        int p = n;
+        while(n>0){
+            if(n%2==0){
+                x = x*x;
+                n = n/2;
             }
             else{
-                x = x*x;
-                nn = nn/2;
+                answer = answer * x;
+                n = n-1;
             }
         }
-        if(n<0) ans = 1/ans;    // use to hadle if any negative power there than divide the answer.
-        
-        return ans;
+        if(nn<0) return 1/answer;
+        return answer;
     }
 };
