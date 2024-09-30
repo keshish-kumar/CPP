097831de89499fe1bfa85020731ab7089816b89d
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-09-30 12:08:06 +0530

    Time: 12 ms (83.74%), Space: 17 MB (89.02%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int> ans;
+        // inddegree vertices.
+        vector<int> indegree(n,0);
+        vector<vector<int > > adj(n);
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            indegree[pre[i][0]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+            for(auto it:  adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+        if(ans.size()!=n) return {};
+        return ans;
+        
+    }
+};
