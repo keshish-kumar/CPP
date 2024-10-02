6e2ca64c1070f2bf312430e54d84ebfa15d37957
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-02 22:22:10 +0530

    Time: 3 ms (85.58%), Space: 16.7 MB (42.26%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,15 +5,16 @@ public:
         int ans = INT_MIN;
         for(int i=0;i<nums.size();i++){
             prefix = prefix*nums[i];
-            ans = max(ans,prefix);
-            if(nums[i]==0) prefix = 1;
+            ans= max(ans,prefix);
+            //ans = max(ans,prefix);
+            if(nums[i]==0) prefix=1;
         }
-
         int suffix = 1;
         for(int i=nums.size()-1;i>=0;i--){
-            suffix = suffix*nums[i];
-            ans= max(ans,suffix);
-            if(nums[i]==0) suffix =1;
+            suffix *=nums[i];
+            ans = max(ans,suffix);
+           // ans = max(ans,suffix);
+            if(nums[i]==0) suffix=1;
         }
         return ans;
     }
