52abbfe9157548520f111b3f26e5eb31a0f3a74b
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-06 14:47:59 +0530

    Time: 11 ms (73.3%), Space: 7.9 MB (75.49%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    bool isvalid(vector<vector<char>>& board, int i, int j, char k){
-        for(int l=0;l<9;l++){
-            if(board[i][l]==k) return false;
-            if(board[l][j]==k) return false;
-            if(board[3*(i/3)+l/3][3*(j/3)+l%3]==k) return false;
+    bool ispossible(vector<vector<char>>& board, int row, int col, char ch){
+
+        for(int i=0;i<9;i++){
+            // check for row
+            if(board[i][col]== ch) return false;
+            // check for col
+            if(board[row][i] == ch) return false;
+            // check for box
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch) return false;
+
         }
         return true;
     }
-    
+
     bool solve(vector<vector<char>>& board){
-        
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                
-                if(board[i][j]=='.'){
-                    for(char k='1';k<='9';k++){
-                        
-                        if(isvalid(board,i,j,k)){
-                           
-                                board[i][j]= k;
-                                if(solve(board)) return true;
-                                else board[i][j] = '.';
-                            }
-                        
-                    }
-                    return false;
-                    
+
+
+        // we have to to traverse for each colum of that row and chck wether we can put numbers from 1 to 9 inn that 
+        for(int row=0;row<9;row++){
+        for(int col=0;col<9;col++){ // col
+            if(board[row][col]=='.'){
+            for(char i='1';i<='9';i++){
+                if( ispossible(board,row,col,i)){ // we will check wether we can put number i in that position
+                    board[row][col] = i;
+                    if(solve(board)) return true;
+                    else board[row][col] = '.';
                 }
-                
+               
             }
+              return false; 
+        }
+        }
         }
         return true;
     }
-    
-    
-    
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+         // We are given boards in which number and . are given
+         // we have to check wether number from 1to 9 can bee filled inside this or not
+         // for this we will use backtracking
+         solve(board); // we will start with 0 row;
     }
 };
