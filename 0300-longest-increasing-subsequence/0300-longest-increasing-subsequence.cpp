91c46ef2e8dbc602d00eab9e2345fa06afa89bb2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-07 22:48:35 +0530

    Time: 93 ms (47.18%), Space: 13.2 MB (51.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int prev, int index,vector<vector<int>>& dp){
-        // base condition
-        if(index>=nums.size()){
-        
-           return 0;
+    int solve(vector<int>& nums, int prev, int index){
+        if(index==nums.size()){
+            return 0;
         }
-
-        if(dp[index][prev+1]!=-1) return dp[index][prev+1];
-        // pick that element
-        int pick = INT_MIN;
-        if(prev==-1 || nums[prev]<nums[index]){
-
-            pick = 1+ solve(nums,index,index+1,dp);
-            
+        // take
+        int ans = INT_MIN;
+        if(nums[index]>prev){
+            ans = 1+solve(nums,nums[index],index+1);
         }
-
-        // not picking that element
-        int notpick = solve(nums,prev,index+1,dp);
-        return dp[index][prev+1] = max(pick,notpick);
-
+        ans = max(ans,solve(nums,prev,index+1));
+        return ans;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(),1);
-        for(int i=0;i<nums.size();i++){
+        // here we will assign previous eloement for comparision
+        // int prev = INT_MIN;
+        // return solve(nums,prev,0);
+        
+        // 2d dp
+       vector<int> dp(nums.size(),1);
+        for(int i=1;i<nums.size();i++){
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
-                    int n = dp[j]+1;
-                    dp[i] = max(dp[i],n);
+                    dp[i] = max(dp[i],dp[j]+1);
                 }
             }
         }
-        int ans = 0;
+
+        int ans =0;
         for(int i=0;i<nums.size();i++){
             ans = max(ans,dp[i]);
         }
+
         return ans;
     }
 };
