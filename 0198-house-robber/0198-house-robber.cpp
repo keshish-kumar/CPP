1c78a8f907516526ed5066ad80e5d51920b64ace
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-08 20:09:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (46.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-
-    int house(vector<int>& nums, int i,vector<int>& dp){
-        if(i>=nums.size()) return 0;
-        
-        if(dp[i]!=-1) return dp[i];
-       int steal = nums[i]+ house(nums,i+2,dp);
-       int skip = house(nums,i+1,dp);
-        return dp[i] = max(steal,skip);
-    }    
-
-    int rob(vector<int>& nums) {
-        // House robber
-        vector<int> dp(nums.size()+1,0);
-        //return house(nums,0,dp);
-        dp[0] = 0; // when no house is present;
-
-        dp[1] = nums[0]; // when only one house is present
-
-        for(int i=2;i<=nums.size();i++){
-
-            int steal = nums[i-1]+ dp[i-2]; // nums[i-1] beacuse that is. the index for that position as it is starting forom 0
-            int skip = dp[i-1];
-            dp[i] = max(steal,skip);
+    int solve(vector<int>& nums, int index){
+        if(index>=nums.size()) return 0;
+        int ans = -1e9;
+        for(int i=index;i<nums.size();i++){
+            // tke that element
+            int temp =  max(nums[i]+solve(nums,i+2),solve(nums,i+1));
+            ans = max(ans,temp);
         }
+        return ans;
+    }
+    int rob(vector<int>& nums) {
+        //return solve(nums,0);
+        vector<int> dp(nums.size()+1,0);
+        dp[1] = nums[0];
+            for(int i=2;i<=nums.size();i++){
+            // tke that element
+                int temp =  0;
+                int temp1 = 0;
+                if((i-2)>=0){
+                    temp1 = nums[i-1]+dp[i-2];
+                }
+                if((i-1)>=0) temp = dp[i-1];
+                dp[i] = max(temp1,temp);
+            }
         return dp[nums.size()];
-        
     }
 };
