2036958e3a9b32c4475eb89bee512bec32b685f2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-08 12:43:16 +0530

    Time: 19 ms (80.79%), Space: 26 MB (32.14%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int solve(string & text1, string &text2, int i, int j){
+        if(i>=text1.size() || j>=text2.size()) return 0;
+
+        // element mathced
+        int ans = -1e9;
+        if(text1[i]==text2[j]){
+            return  1+solve(text1,text2,i+1,j+1);
+        }
+        
+        return max(solve(text1,text2,i,j+1),solve(text1,text2,i+1,j));
+        
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        //return solve(text1,text2,0,0);
+
+        // Now we will try doing it using 2d dp
+        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                // if j=1 and i=1 means 1 elemt is prsent in text2 and i element present in text1
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[text1.size()][text2.size()];
+    }
+};
