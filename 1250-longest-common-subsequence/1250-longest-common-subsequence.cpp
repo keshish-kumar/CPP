e46a142ad2e1e3bdd318345464f5d2c3681663d0
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-08 12:47:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -16,19 +16,35 @@ public:
         //return solve(text1,text2,0,0);
 
         // Now we will try doing it using 2d dp
-        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        //  vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        // for(int i=1;i<=text1.size();i++){
+        //     for(int j=1;j<=text2.size();j++){
+        //         // if j=1 and i=1 means 1 elemt is prsent in text2 and i element present in text1
+        //         if(text1[i-1]==text2[j-1]){
+        //             dp[i][j] = 1+dp[i-1][j-1];
+        //         }
+        //         else{
+        //             dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        //         }
+        //     }
+        // }
+
+        // return dp[text1.size()][text2.size()];
+
+        // Now we will try using sinle dp
+        vector<int> dp(text2.size()+1,0);
         for(int i=1;i<=text1.size();i++){
+            vector<int> temp(text2.size()+1,0);
             for(int j=1;j<=text2.size();j++){
-                // if j=1 and i=1 means 1 elemt is prsent in text2 and i element present in text1
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    temp[j] = 1+temp[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    temp[j] = max(dp[j],temp[j-1]);
                 }
             }
+            dp=temp;
         }
-
-        return dp[text1.size()][text2.size()];
+        return dp[text2.size()];
     }
 };
