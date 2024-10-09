b1c6680b1e40da739aeb42fa187343b8e557b584
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-09 11:41:06 +0530

    Time: 7 ms (85.24%), Space: 15 MB (19.44%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    
-    int path(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
-        if(i==0 && j==0 ) return grid[i][j];
-        
-        int left = 1e9, right = 1e9;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(i>0) left =  path(grid,i-1,j,dp);
-        if(j>0) right =  path(grid,i,j-1,dp);
-        return  dp[i][j] = grid[i][j]  + min(left,right);
-    }
-    
-    int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,1e9));
-       // return path(grid,n-1,m-1,dp);
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0)  dp[i][j] = grid[i][j];
-                else{
-                    int left = 1e9,right = 1e9;
-                    if(i>0) left = dp[i-1][j];
-                    if(j>0) right = dp[i][j-1];
-                    dp[i][j] = grid[i][j] + min(left,right);
-                }
-            }
+    int solve(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
+        if(i==grid.size()-1 && j==grid[0].size()-1){
+            return grid[i][j];
         }
-        return dp[n-1][m-1];
+        if(i>=grid.size() || j>=grid[0].size()){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        // right side
+        int right = INT_MAX;
+        if((j+1)<grid[0].size()){
+            right = grid[i][j] + solve(grid,i,j+1,dp);
+        }
+
+        // down side
+        int down = INT_MAX;
+        if((i+1)<grid.size()){
+            down = grid[i][j] + solve(grid,i+1,j,dp);
+        }
+        return dp[i][j] = min(right,down);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        // we will use recursion 
+        vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        return solve(grid,0,0,dp);
     }
 };
