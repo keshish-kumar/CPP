d66cb91decb59a012138ec2695e9833e1706387a
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-09 12:00:23 +0530

    Time: 12 ms (44.22%), Space: 14.8 MB (40.56%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,9 +4,7 @@ public:
         if(i==grid.size()-1 && j==grid[0].size()-1){
             return grid[i][j];
         }
-        if(i>=grid.size() || j>=grid[0].size()){
-            return 0;
-        }
+     
         if(dp[i][j]!=-1) return dp[i][j];
 
         // right side
@@ -24,7 +22,23 @@ public:
     }
     int minPathSum(vector<vector<int>>& grid) {
         // we will use recursion 
+        //vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
+        //return solve(grid,0,0,dp);
+
         vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        return solve(grid,0,0,dp);
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(i==0 && j==0) dp[i][j] = grid[i][j];
+                else{
+                    int left = 1e9,right=1e9;
+                    if(i>0) left = dp[i-1][j];
+                    if(j>0) right = dp[i][j-1];
+                    dp[i][j] = grid[i][j] + min(left,right);
+                }
+            }
+        }
+
+        return dp[grid.size()-1][grid[0].size()-1];
+
     }
 };
