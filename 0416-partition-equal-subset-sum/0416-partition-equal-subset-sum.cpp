0338c45ce24e537010a60e7b5c0f2e89f17f3bb4
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-09 14:59:12 +0530

    Time: 222 ms (36.33%), Space: 241.2 MB (6.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,56 +1,18 @@
 class Solution {
 public:
-    bool partition(vector<int>& nums, int sum, int n , vector<vector<int>>& dp){
-        
-        if(sum == 0) return true;
-        
-        if(n==0 ) return nums[n]==sum;
-        
-        if(dp[n][sum]!=-1) return dp[n][sum];
-        
-        int notpick = partition(nums,sum,n-1,dp);
-        
-        int pick = false;
-        
-        if(sum>=nums[n])
-                pick = partition(nums,sum-nums[n],n-1,dp);
-        
-        return dp[n][sum] = notpick||pick;
-        
+    bool solve(vector<int>& nums, int i, int sum,int acc,vector<vector<int>>& dp){
+        if(i>=nums.size()) return false;
+
+        if(sum-acc == acc) return true;
+        if(dp[i][acc]!=-1) return dp[i][acc];
+        //pick
+        return dp[i][acc] =  solve(nums,i+1,sum,acc+nums[i],dp) || solve(nums,i+1,sum,acc,dp);
+
     }
     bool canPartition(vector<int>& nums) {
-        
         int sum = 0;
-        int n = nums.size();
-        for(int i=0;i<nums.size();i++)
-                sum += nums[i];
-        
-       
-        
-        if(sum%2 != 0) return false;
-         sum = sum/2;
-//         vector<vector<int>> dp(n,vector<int>(sum+1 ,-1));
-        
-//         return partition(nums,sum,n-1,dp);
-        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));
-        
-        for(int i=0;i<=n;i++)
-            dp[i][0] = true;    // this indicates that when sum is 0 its always true
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                
-                bool pick = dp[i-1][j];
-                bool notpick = false;
-                if(j>=nums[i-1]){
-                    notpick = dp[i-1][j-nums[i-1]];
-                }
-                dp[i][j] = pick||notpick;
-            }
-        }
-        
-            
-       return dp[n][sum];     
-        
+        for(int i=0;i<nums.size();i++) sum += nums[i];
+        vector<vector< int >> dp(nums.size(),vector<int>(sum,-1));
+        return solve(nums,0,sum,0,dp);
     }
 };
