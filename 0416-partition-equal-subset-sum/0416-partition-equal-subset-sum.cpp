32c0784c706d6bda2b9e879f813466730f138a41
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-09 15:46:31 +0530

    Time: 275 ms (29.02%), Space: 16.4 MB (76.75%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -12,7 +12,28 @@ public:
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int i=0;i<nums.size();i++) sum += nums[i];
-        vector<vector< int >> dp(nums.size(),vector<int>(sum,-1));
-        return solve(nums,0,sum,0,dp);
+        //vector<vector< int >> dp(nums.size(),vector<int>(sum,-1));
+        //return solve(nums,0,sum,0,dp);
+
+        // we will use direclty sapce optimized approach
+        if (sum % 2 != 0) return false;
+        sum = sum/2;
+        vector<bool> dp(sum+1,false);
+        dp[0] = true;
+        for(int i=1;i<=nums.size();i++){
+            vector<bool> temp(sum+1,false);
+            temp[0] = true;
+            for(int j=1;j<=sum;j++){
+               // if(sum-j == j) return true;
+                bool pick = dp[j];
+                bool notpick = false;
+                if(j>=nums[i-1])
+                    notpick = dp[j-nums[i-1]];
+                temp[j] = pick||notpick;
+            }
+            dp = temp;
+        }
+
+        return dp[sum];
     }
 };
