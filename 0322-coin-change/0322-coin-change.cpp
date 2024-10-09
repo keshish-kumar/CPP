ea6635bbca472aeffc20afe4a1fefbb8b97cb6a7
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-09 12:34:36 +0530

    Time: 48 ms (30.08%), Space: 46.9 MB (43.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -10,10 +10,29 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         // this will return the sorted order in reverse
-        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
-        int ans = solve(coins,amount,0,dp);
-        if(ans==1e9) return -1;
-        else return ans;
+        // vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,-1));
+        // int ans = solve(coins,amount,0,dp);
+        // if(ans==1e9) return -1;
+        // else return ans;
 
+        // Now we will look for space optimized approach
+
+        vector<int> dp(amount+1,1e9);
+        dp[0] = 0;
+        for(int i=1;i<=coins.size();i++){
+            vector<int> temp(amount+1,1e9);
+            for(int j=0;j<=amount;j++){
+                int left = 1e9;
+                if(coins[i-1]<=j){
+                    left = 1+temp[j-coins[i-1]];
+                }
+                int right = dp[j];
+
+                temp[j] = min(left,right);
+
+            }
+            dp = temp;
+        }
+        return (dp[amount]==1e9)?-1:dp[amount];
     }
 };
