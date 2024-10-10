d8be0d52506e6a42c32cc6ef8cb3daaffd45db24
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-10 17:42:54 +0530

    Time: 57 ms (13.78%), Space: 39.4 MB (11.24%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int i=0;
-        int j=0;
-        int sum=0;
-        int ans =0;
-        int count=0;
-        while(j<nums.size()){
-            sum += nums[j];
-            while(i<j && (nums[i]==0 || sum>goal)){
-                if(nums[i]==0) count++;
-                else count=0;
-                sum = sum-nums[i];
-                i++;
+        // int i=0;
+        // int j=0;
+        // int sum=0;
+        // int ans =0;
+        // int count=0;
+        // while(j<nums.size()){
+        //     sum += nums[j];
+        //     while(i<j && (nums[i]==0 || sum>goal)){
+        //         if(nums[i]==0) count++;
+        //         else count=0;
+        //         sum = sum-nums[i];
+        //         i++;
+        //     }
+        //     if(sum==goal){
+        //         ans += 1+count;
+        //     }
+        //     j++;
+        // }
+        // return ans;
+
+        map<int,int> mp;
+        int ans =0,sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(sum == goal) ans++;
+            if(mp.find(sum-goal)!=mp.end()){
+                ans = ans+mp[sum-goal];
             }
-            if(sum==goal){
-                ans += 1+count;
-            }
-            j++;
+            mp[sum]++;
         }
-        return ans;
+    return ans;
     }
 };
