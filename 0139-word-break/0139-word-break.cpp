416ff66f845ba761434a5aabcd493300f109a210
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-11 13:31:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    bool solve(string & s, set<string>& st, int i,vector<int>& dp){
-         if(i>=s.size()) return true;
-        if(dp[i]!=-1) return dp[i];
-         for(int k=i;k<s.size();k++){
-            if(st.find(s.substr(i,k-i+1))!=st.end() && solve(s,st,k+1,dp)){
-                return dp[i] = true;
+    bool solve(string & s, set<string>& st, int i){
+        if(i>=s.size()) return true;
+        for(int k=i;k<s.size();k++){
+            if(st.find(s.substr(i,k-i+1))!=st.end() && solve(s,st,k+1)){
+                return true;
             }
-         }
-         return dp[i] =  false;
 
-
+        }
+        return false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         set<string> st;
-        for(auto it:wordDict){
+        for(string it:wordDict){
             st.insert(it);
         }
-        vector<int> dp(s.size(),-1);
-        return solve(s,st,0,dp);
+        return solve(s,st,0);
     }
 };
