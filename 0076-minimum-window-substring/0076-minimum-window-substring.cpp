214d741f919d644ec59aa2d6f99902017fb47bca
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-11 17:55:44 +0530

    Time: 29 ms (34.24%), Space: 11.2 MB (27.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.size()<t.size()) return "";
         map<char,int> mp;
-        for(int i=0;i<t.size();i++){
-            mp[t[i]]++;
-        }
-         
-        int i=0,j=0,count=t.size(),ans1=1e9;string ans=s;
-        int starti=0;
-
-        while(i<s.size()){
-            
-            if(mp[s[i]]>0) count--;
-            mp[s[i]]--;
-
-
-           while(count==0){
-            if(ans1>(i-j+1)){
-                ans1 = (i-j+1);
-                starti = j;
+        for(char it:t)
+            mp[it]++;
+        int count = t.size();
+        int i=0,j=0,ans=INT_MAX;int str = -1;
+        while(j<s.size()){
+           
+            if(mp[s[j]]>0){ // important to check before reducing it 
+                count--; 
             }
-            mp[s[j]]++;
-
-            if(mp[s[j]]>0) count++;
-            j++;
-           }
-           i++;
+            mp[s[j]]--;
+            while(count==0){
+               // cout<<s.substr(i,j-i+1);
+                if(ans>(j-i+1)){
+                    str = i;
+                    ans = j-i+1;
+                }
+                mp[s[i]]++;
+                if(mp[s[i]]>0) count++;
+                i++;
+            }
+             j++;
         }
-        return ans1==1e9?"":s.substr(starti,ans1);
 
+       
+        return str==-1?"": s.substr(str,ans);
     }
 };
