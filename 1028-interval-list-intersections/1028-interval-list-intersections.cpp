da4144afdecec72578d1421e8f9ae5d9d1473fba
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-13 17:09:02 +0530

    Time: 27 ms (32.56%), Space: 22.6 MB (66.84%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.cpp
+++ 1028-interval-list-intersections/1028-interval-list-intersections.cpp
@@ -4,10 +4,10 @@ public:
         vector<vector<int>> ans;
         int i=0,j=0;
         while(i<firstList.size() && j<secondList.size()){
-            // if((firstList[i][1]<=secondList[j][1] || firstList[i][1]>=secondList[j][1]) &&(firstList[i][0]<=secondList[j][0] || firstList[i][0]>=secondList[i][0])){
-                if(max(firstList[i][0],secondList[j][0])<=min(firstList[i][1],secondList[j][1]))
+            
+                if (firstList[i][0] <= secondList[j][1] && secondList[j][0] <= firstList[i][1])
                  ans.push_back({max(firstList[i][0],secondList[j][0]),min(firstList[i][1],secondList[j][1])});
-           // }
+ 
             if(firstList[i][1]<secondList[j][1]) i++;
             else j++;
         }
