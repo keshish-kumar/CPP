41399f007dd6bca17851faa83574ca60da5794cc
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-13 13:14:54 +0530

    Time: 9 ms (73.85%), Space: 20.8 MB (42.16%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& interval, vector<int>& newInterval) {
-        
-        // first we will tryn to merge the intervals;
-        vector<vector<int>> v; 
-        
-        if(interval.size()==0) {v.push_back(newInterval);return v;}
-        int i=0,j=0;
-        
-       bool flag =true;
-        while(j<interval.size()){
-            
-            if(flag && newInterval[1]<interval[j][0]){
-                v.push_back(newInterval);
-                flag=false;
-            }
-             if(flag && newInterval[0]<=interval[j][1]){
-                int x=j;
-                
-                int maxi = max(newInterval[1],interval[x][1]);
-                x++;
-                while(x<interval.size() && interval[x][0]<=newInterval[1]){
-                    maxi = max(interval[x][1],newInterval[1]);
-                    x++;
-                }
-                
-                v.push_back({min(interval[j][0],newInterval[0]),maxi});
-                j=x;
-                flag=false;
-            }
-            
-            else{
-                v.push_back(interval[j]);
-                j++;
-                
-            }
-            
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        // first check element before newIntervals
+        int i=0;
+        while(i<intervals.size() && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
         }
-        
-        if(flag) v.push_back(newInterval);
-        return v;
+        // Here we have to check for newIntervals
+        while(i<intervals.size() && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+
+        while(i<intervals.size()){
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
     }
 };
