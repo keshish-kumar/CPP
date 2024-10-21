545ebae4f72d16a37800f3d64f09c7cdc13ead46
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-21 21:57:16 +0530

    Time: 3 ms (99.03%), Space: 35.3 MB (45.55%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,7 +2,20 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         vector<int> ans;
-        int n = lower_bound(arr.begin(),arr.end(),x)-arr.begin()-1;
+       // int n = lower_bound(arr.begin(),arr.end(),x)-arr.begin()-1;
+       // Now we will write function to find lowe bound of that elment
+       int low = 0,high = arr.size()-1;
+       int n=-1;
+       while(low<=high){
+        int mid = (low+high)/2;
+
+        if(arr[mid]>x){
+            
+            high = mid-1;
+        } 
+        else { n=mid;low = mid+1;}
+       }
+       
         cout<<n<<endl;
         int i=n,j=n+1;
        
