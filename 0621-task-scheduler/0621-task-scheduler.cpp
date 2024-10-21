890cbc42f87a67bc77c3e7d6af0449f17fb10de6
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-21 13:03:22 +0530

    Time: 0 ms (100%), Space: 38.3 MB (68.63%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -4,36 +4,19 @@ public:
     // O(n)
     // priority_queu log(priority_queue size()) , log(26)
     int leastInterval(vector<char>& tasks, int n) {
-        map<char, int> mp;
-        for(int i=0;i<tasks.size();i++) mp[tasks[i]]++;
-        priority_queue<int> pq;
-        for(auto it:mp) pq.push(it.second);
-        int ans=0;
-        while(!pq.empty()){
-            // we have to push it till n times.
-           queue<int> q;
-           // we have to run this loop for n+1 times if n=2 than A,B,idle after A we need two more cycle 
-           for(int i=1;i<=n+1;i++){
-              if(!pq.empty()){
-                q.push(pq.top());
-                pq.pop();
-              }
-           }
+       vector<int> v(26,0);
+       for(int i=0;i<tasks.size();i++)
+        v[tasks[i]-'A']++;
+        sort(v.begin(),v.end()); // sort this
 
-            int temp = q.size();
-            // than we have to push element in q if >=1 , 
-            while(!q.empty()){
-                int t = q.front()-1;
-                q.pop();
-                if(t>0)
-                    pq.push(t);
-            }
+        int maxfreq = v[25];
+        int gddhe = maxfreq-1;
+        int idleslot = n*gddhe;
 
-            if(!pq.empty()) // if pq is not empty than it will take n+1 cycle
-                ans += n+1;
-            else ans += temp; // if empty than it ends and we have to return only last eleemnt not the idle time.
-
+        for(int i=24;i>=0;i--){
+            idleslot -= min(v[i],gddhe);
         }
-        return ans;
+        if(idleslot<=0) return tasks.size();
+        else return tasks.size()+idleslot;
     }
 };
