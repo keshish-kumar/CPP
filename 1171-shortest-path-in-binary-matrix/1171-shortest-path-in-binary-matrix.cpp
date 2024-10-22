a5edcd374d5a01a423b2e77b721aec8f8ee43ce6
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-10-22 18:49:04 +0530

    Time: 39 ms (85.47%), Space: 23.5 MB (78.67%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        // Now I will use dijkstras to solve this question.
+        
         int n = grid.size();
         int m = grid[0].size();
         if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
-
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        grid[0][0]=1;
-        int level=1;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> q; // one is to store distance and other is to store the index
+        vector<vector<int>> distance(n,vector< int>(m,1e9));
+        q.push({1,{0,0}});
+        distance[0][0]=1;
+        vector<int> ai = {0,1,1,1,0,-1,-1,-1};
+        vector<int> aj = {1,1,0,-1,-1,-1,0,1};
         while(!q.empty()){
             int size = q.size();
-           
             while(size--){
-                int i= q.front().first;
-                int j = q.front().second;
+                int i= q.top().second.first;
+                int j = q.top().second.second;
+                int dist = q.top().first;
                 q.pop();
-                vector<int> ai={0,1,1,1,0,-1,-1,-1};
-                vector<int> aj={1,1,0,-1,-1,-1,0,1};
-                if(i==n-1 && j==m-1) return level;
-                
+                if(i==n-1 && j==m-1) return dist;
                 for(int k=0;k<8;k++){
                     int ii=i+ai[k];
                     int jj=j+aj[k];
                     if(ii>=0 && jj>=0 && ii<n && jj<m && grid[ii][jj]==0){
-                            q.push({ii,jj});
-                            grid[ii][jj]=1;
-                            
+                         if(distance[ii][jj]>(dist+1)){
+                            distance[ii][jj] = dist+1;
+                            q.push({distance[ii][jj],{ii,jj}});
+                         }
+                         grid[ii][jj]=1;
                     }
-                } 
+                }
             }
-             level++;
         }
         return -1;
     }
