1a91125963f49cd3cbb0594eea59ef071f48313a
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-07 16:43:56 +0530

    Time: 84 ms (43.72%), Space: 13.2 MB (48.92%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int prev, int index){
-        if(index==nums.size()){
+    int solve(vector<int>& nums, int index, int prev){
+        if(index>=nums.size()){
             return 0;
         }
-        // take
-        int ans = INT_MIN;
-        if(nums[index]>prev){
-            ans = 1+solve(nums,nums[index],index+1);
+
+        int l1 = INT_MIN;
+        if( nums[index]>prev){
+            l1 = 1+solve(nums,index+1,nums[index]);
         }
-        ans = max(ans,solve(nums,prev,index+1));
-        return ans;
+        int l2 = solve(nums,index+1,prev);
+
+        return max(l1,l2);
     }
     int lengthOfLIS(vector<int>& nums) {
-        // here we will assign previous eloement for comparision
-        // int prev = INT_MIN;
-        // return solve(nums,prev,0);
-        
-        // 2d dp
-       vector<int> dp(nums.size(),1);
+        // first we ill try with pick and not pick 
+
+        //return solve(nums,0,INT_MIN);
+        vector<int> dp(nums.size(),1);
         for(int i=1;i<nums.size();i++){
             for(int j=0;j<i;j++){
                 if(nums[j]<nums[i]){
-                    dp[i] = max(dp[i],dp[j]+1);
+                    dp[i]=max(dp[i],dp[j]+1);
                 }
             }
         }
-
         int ans =0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++)
             ans = max(ans,dp[i]);
-        }
-
         return ans;
     }
 };
