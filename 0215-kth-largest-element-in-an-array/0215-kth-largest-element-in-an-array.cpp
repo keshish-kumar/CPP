26af1c88b2d1b5700bb82b861bbce4cc3694b8c2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-07 15:06:06 +0530

    Time: 51 ms (25.41%), Space: 61.5 MB (29.05%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,10 +4,9 @@ public:
         priority_queue<int,vector<int>,greater<int>> pq;
         for(int i=0;i<nums.size();i++){
             pq.push(nums[i]);
+            if(pq.size()>k) pq.pop();
         }
-        while(pq.size()!=k){
-            pq.pop();
-        }
+       
         return (int)pq.top();
     }
 };
