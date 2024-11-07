2d247e85426b85ef8668e54d22b97330f72fe320
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-07 16:27:15 +0530

    Time: 0 ms (100%), Space: 14.8 MB (68.8%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
-        if(i==grid.size()-1 && j==grid[0].size()-1){
-            return grid[i][j];
-        }
-     
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        // right side
-        int right = INT_MAX;
-        if((j+1)<grid[0].size()){
-            right = grid[i][j] + solve(grid,i,j+1,dp);
-        }
-
-        // down side
-        int down = INT_MAX;
-        if((i+1)<grid.size()){
-            down = grid[i][j] + solve(grid,i+1,j,dp);
-        }
-        return dp[i][j] = min(right,down);
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        // we will use recursion 
-        //vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        //return solve(grid,0,0,dp);
-
-        //vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        vector<int> dp(grid[0].size(),-1);
-        for (int j = 0; j < grid[0].size(); j++) {
-        if (j == 0) {
-            dp[j] = grid[0][j];
-        } else {
-            dp[j] = dp[j - 1] + grid[0][j];
+        int n = grid.size(); int m = grid[0].size();
+        vector<int> dp(m,0);
+        dp[0] = grid[0][0];
+        for(int i=1;i<m;i++){
+            dp[i] = dp[i-1]+grid[0][i];
         }
-    }
-        for(int i=1;i<grid.size();i++){
-            vector<int> temp(grid[0].size(),-1);
-            for(int j=0;j<grid[0].size();j++){
-                 if (j == 0) {
-                temp[j] = dp[j] + grid[i][j];  // From the top cell
-                } else {
-                temp[j] = grid[i][j] + min(dp[j], temp[j - 1]);  // Min from top or left
+
+        for(int i=1;i<n;i++){
+            vector<int> temp(m,0);
+            for(int j=0;j<m;j++){
+                if(j==0){temp[j] = grid[i][j]+dp[j];}
+                else{
+                    temp[j] = min(dp[j],temp[j-1])+grid[i][j];
                 }
             }
-            dp =temp;
+            dp = temp;
         }
-
-        return dp[grid[0].size()-1];
-
+        return dp[m-1];
     }
 };
