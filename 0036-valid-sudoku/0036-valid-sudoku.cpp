645de19d11d47ec51e2eccd3f8144c9ec2c2e013
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-08 19:10:49 +0530

    Time: 1 ms (72.17%), Space: 21.9 MB (95.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,61 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // validate col
+            for(int i=0;i<9;i++){
+                for(int j=0;j<9;j++){
+                    if(board[i][j]>='1' && board[i][j]<='9'){
+                        for(int k=0;k<9;k++){
+                            if(board[i][k]==board[i][j] && k!=j) return false;
+                            if(board[k][j]==board[i][j] && k!=i) return false;
+                            if(board[3*(i/3)+k/3][3*(j/3)+k%3]==board[i][j] &&i!=3*(i/3)+k/3 && j!=3*(j/3)+k%3 ) return false;
+                        }
 
-        // for(int i=0;i<9;i++){ // col fixed
-        //     set<char> st;
-        //     for(int j=0;j<9;j++){ // rows changing
-        //         if(st.find(board[j][i])!=st.end()) return false;
-        //         if(board[j][i]>='0' && board[j][i]<='9') st.insert(board[j][i]);
-        //     }
-        // }
-        // cout<<"col checked"<<endl;
-        // // validate row
-
-        // for(int i=0;i<9;i++){ // row fixed
-        //     set<char> st;
-        //     for(int j=0;j<9;j++){ // col changing
-        //         if(st.find(board[i][j])!=st.end()) return false;
-        //         if(board[i][j]>='0' && board[i][j]<='9') st.insert(board[i][j]);
-        //     }
-        // }
-        // cout<<"row checked"<<endl;
-        // // validate. 3*3 boxes
-
-        // for(int i=0;i<9;i=i+3){
-        //     for(int j=0;j<9;j=j+3){
-        //         set<char> st;
-        //         for(int row=i;row<(i+3);row++){
-        //             for(int col = j ;col<(j+3);col++){
-        //                 if(st.find(board[row][col])!=st.end()) return false;
-        //                 if(board[row][col]>='0' && board[row][col]<='9') st.insert(board[row][col]);
-        //             }
-        //         }
-        //     }
-        // }
-        // cout<<"boxes checked"<<endl;
-        // return true;
-        set<string> st;
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                // row
-                string row = to_string(i)+"row"+board[i][j];
-                if(st.find(row)!=st.end()) return false;
-                st.insert(row);
-                
-                string col = to_string(j)+"col"+board[i][j];
-                if(st.find(col)!=st.end()) return false;
-                st.insert(col);
-                
-                string box = to_string(i/3)+to_string(j/3)+"box"+board[i][j];
-                if(st.find(box)!=st.end()) return false;
-                st.insert(box);
-                
+                    }
                 }
             }
-        }
-        return true;
+            return true;
     }
 };
