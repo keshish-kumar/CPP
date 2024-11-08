1511d21047d8ed283fae10c2aafa16ce0e13e41f
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-08 17:41:32 +0530

    Time: 13 ms (5.38%), Space: 96.5 MB (7.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& arr1, vector<int>& arr2) {
-        if(arr1.size()>arr2.size()) swap(arr1,arr2);
-        int x = arr1.size(), y = arr2.size();
-      
-        int low = 0, high =arr1.size();
-        int k = (x+y)/2;
-        while(low<=high){
-            int mid = (low+high)/2; // no of lelemnt from arr1 in leftpart
-            
-            // Now we will fill the leftpart
-            // we now know that mid is the number of elemt present in pargt1 from arr1
-            // we can get number of element present in part1 from arr2 as k-mid;
-            int x1 = (mid>0)?arr1[mid-1]:-1e9;
-            int y1 = ((k-mid)>0 )?arr2[k-mid-1]:-1e9;
-            
-            // Now we will fill the rightpart
-            int x2 = (mid<x)?arr1[mid]:1e9;
-            int y2 = (k-mid<y)?arr2[k-mid]:1e9;
-            
-            if(x1<=y2 && y1<=x2){
-                if((x+y)%2==0){
-                    return (max(x1,y1)+min(x2,y2))/2.0;
-                }
-                else
-                    return min(x2,y2);
-            }
-            
-            if(x1>y2){
-                high = mid-1;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        priority_queue<int> left;
+        priority_queue<int,vector<int>,greater<int>> right;
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<nums2[j]){
+                right.push(nums1[i]);
+                i++;
             }
             else{
-                low = mid+1;
+                right.push(nums2[j]);
+                j++;
+            }
+
+            if((right.size()-left.size()) >1){
+                left.push(right.top());
+                right.pop();
             }
         }
-        return -1;
+
+        while(i<nums1.size()){
+            right.push(nums1[i]);
+            if((right.size()-left.size())>1){
+                left.push(right.top());
+                right.pop();
+            }
+            i++;
+        }
+
+        while(j<nums2.size()){
+            right.push(nums2[j]);
+            if((right.size()-left.size())>1){
+                left.push(right.top());
+                right.pop();
+            }
+         j++;
+        }
+
+        if((right.size()-left.size())==1){
+            return right.top();
+        }
+        else{
+            return (right.top()+left.top())/2.00;
+        }
     }
 };
