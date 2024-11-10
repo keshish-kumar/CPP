d4be1ea9f01ec92859623c13f9a3599eaa3b2d2b
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-10 17:58:49 +0530

    Time: 34 ms (78.12%), Space: 17.2 MB (96.14%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,69 +9,18 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // unordered_set<ListNode*> st;
-        // ListNode* temp=headA;
-        // while(temp!=NULL){
-        //     st.insert(temp);
-        //     temp=temp->next;
-        // }
-
-        // ListNode* temp2=headB;
-        // while(temp2!=NULL){
-        //     if(st.find(temp2)!=st.end()){
-        //             break;
-        //     }
-        //     temp2=temp2->next;
-        // }
-
-        // return temp2;
-
-        // int count1=0;
-        // int count2=0;
-        // ListNode* temp1=headA;
-        // while(temp1!=NULL){
-        //     count1++;
-        //     temp1=temp1->next;
-        // }
-
-        // ListNode* temp2=headB;
-        // while(temp2!=NULL){
-        //     count2++;
-        //     temp2=temp2->next;
-        // }
-        // temp1=headA;
-        // temp2=headB;
-        // if(count1>count2){
-        //     while(count1>count2){
-        //         count1--;
-        //         temp1=temp1->next;
-        //     }
-        // }
-        // else if(count1<count2){
-        //     while(count1<count2){
-        //         count2--;
-        //         temp2=temp2->next;
-        //     }
-        // }
-
-        // while(temp1!=NULL && temp2!=NULL){
-        //     if(temp1==temp2) return temp1;
-        //     temp1=temp1->next;
-        //     temp2=temp2->next;
-        // }
-        // return NULL;
-
+        // we will use slow and fats pointer concept here
         ListNode* temp1=headA;
         ListNode* temp2=headB;
-       if(temp1==NULL || temp2==NULL) return NULL;
-       while(temp1!=temp2){
-        temp1=temp1->next;
-         temp2=temp2->next;
-        if(temp1==temp2) return temp1;
-        if(temp1==NULL) temp1=headB;
-        if(temp2==NULL) temp2=headA;
-       }
-       return temp1;
-
+        if(temp1==NULL || temp2==NULL) return NULL;
+        while(temp1!=temp2){
+            temp1=temp1->next;
+            temp2=temp2->next;
+            if(temp1==temp2) return temp1;
+            if(temp1==NULL) temp1=headB;
+            if(temp2==NULL) temp2=headA;
+        }
+        return temp1;
+        
     }
 };
