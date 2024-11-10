aa44f2e5df595fd126c35eb9ae5a0786952a83c6
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-10 15:35:54 +0530

    Time: 0 ms (100%), Space: 16.6 MB (77.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    
     int maxProduct(vector<int>& nums) {
-        // we will do from both left and right to calcujlate the array
-
-        int count =1;
-        int ans = nums[0];
-        for(int i=0;i<nums.size();i++){
-            count *= nums[i];
-             ans = max(ans,count);
-            if(count ==0 ) count =1;
-           
-        }
-
-        // check from right side
-
-        count =1;
-        for(int i=nums.size()-1;i>=0;i--){
-            count *=nums[i];
-             ans = max(ans,count);
-            if(count ==0 ) count =1;
-           
-        }
-        return ans;
+       // one we have to move from right and other we have to move from left
+       int ans = -1e9;
+       int p=1;
+       for(int i=0;i<nums.size();i++){
+        p *=nums[i];
+        ans = max(ans,p);
+        if(p==0) p=1;
+       }
+       p=1;
+       for(int i=nums.size()-1;i>=0;i--){
+        p *=nums[i];
+        ans = max(ans,p);
+         if(p==0) p=1;
+       }
+       return ans;
     }
 };
