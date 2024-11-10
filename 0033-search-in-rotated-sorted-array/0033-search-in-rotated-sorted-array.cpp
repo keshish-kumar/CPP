f4e2a6b19de22fcfd2d49e97068828b0c8ca7d79
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-10 16:20:49 +0530

    Time: 0 ms (100%), Space: 14 MB (52.32%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        // we will sperate both the array and check in that.
-        // we will search for lower_bound of the array
-        int low = 0,high = arr.size()-1;
+    int search(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(arr[mid]==target) return mid;
-           // check if left side is sorted
-            if(arr[mid]>=arr[low]){
-                if(arr[low]<=target && arr[mid]>=target ){
-                    high=mid-1;
+            if(nums[mid]==target) return mid;
+            // if left half is sorted
+            if(nums[low]<=nums[mid]){
+                    if(nums[low]<=target && nums[mid]>target){
+                        high = mid-1;
+                    }
+                    else{
+                        low=mid+1;
+                    }
+            }
+            else{
+                if(nums[mid]<target && nums[high]>=target){
+                    low=mid+1;
                 }
                 else{
-                   low = mid+1;
+                    high=mid-1;
                 }
-            }
-            else{ // right side sorted array
-                if(arr[mid]<=target && arr[high]>=target){
-                   low = mid+1;
-                }
-                else
-                    high = mid-1;
             }
         }
         return -1;
