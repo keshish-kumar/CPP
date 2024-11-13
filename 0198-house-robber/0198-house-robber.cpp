d5dfa6398b381507d702ec978e947006e239dc63
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-13 18:55:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (12.42%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int index){
-        if(index>=nums.size()) return 0;
-        int ans = -1e9;
-        for(int i=index;i<nums.size();i++){
-            // tke that element
-            int temp =  max(nums[i]+solve(nums,i+2),solve(nums,i+1));
-            ans = max(ans,temp);
-        }
-        return ans;
+    int solve(vector<int>& nums, int i,vector<int>& dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int l = nums[i]+solve(nums,i+2,dp);
+        int r = solve(nums,i+1,dp);
+        return dp[i] = max(l,r);
     }
     int rob(vector<int>& nums) {
-        //return solve(nums,0);
-        vector<int> dp(nums.size()+1,0);
-        dp[1] = nums[0];
-            for(int i=2;i<=nums.size();i++){
-            // tke that element
-                int temp =  0;
-                int temp1 = 0;
-                if((i-2)>=0){
-                    temp1 = nums[i-1]+dp[i-2];
-                }
-                if((i-1)>=0) temp = dp[i-1];
-                dp[i] = max(temp1,temp);
-            }
-        return dp[nums.size()];
+        // pick and not pick concept;
+        vector<int> dp(nums.size(),-1);
+        return solve(nums,0,dp);
     }
 };
