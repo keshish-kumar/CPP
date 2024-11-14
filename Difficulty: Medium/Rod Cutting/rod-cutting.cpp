dc39025ecbc559540df6272c6e915deea3a85007
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 12:00:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -10,26 +10,19 @@ using namespace std;
 
 class Solution{
   public:
-  int solve(int price[] , int n,int w,vector<vector<int>>& dp){
-      if(w==0) return 0;
-      if(n==0){
-          return 0;
-      }
+  int solve(int price[], int n, int w,vector<vector<int>>& dp){
+      if(n==0 || w==0) return 0;
       if(dp[n][w]!=-1) return dp[n][w];
-       int notpick = solve(price,n-1,w,dp);
-       
-       int pick = 0;
-       
-       if(w>=n)
-            pick = price[n-1]+solve(price,n,w-n,dp);
-            
-        return dp[n][w] = max(notpick,pick);
-     
+      int l = solve(price,n-1,w,dp);
+      int r = 0;
+      if(w>=n){
+          r = price[n-1]+solve(price,n,w-n,dp);
+      }
       
+      return dp[n][w]=max(l,r);
   }
     int cutRod(int price[], int n) {
         //code here
-        //try to pcik length in all possible way. 5+3 will give me that length.
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         return solve(price,n,n,dp);
     }
@@ -50,7 +43,9 @@ int main() {
         Solution ob;
 
         cout << ob.cutRod(a, n) << endl;
-    }
+    
+cout << "~" << "\n";
+}
     return 0;
 }
 // } Driver Code Ends
