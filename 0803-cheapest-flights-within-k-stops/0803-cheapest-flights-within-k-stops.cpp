4c62b6124b66d4125a2f596732c4f4c99c5561f4
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 12:33:27 +0530

    Time: 5 ms (38.68%), Space: 16.6 MB (91.82%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq; 
-        // we will make adjacency matrix which will strore path which can be visited from .
         vector<vector<pair<int,int>>> adj(n);
         for(int i=0;i<flights.size();i++){
             adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
-        // Now that we have found adjacancy matrix we will apply dijkstras algorithm as weight is also given.
-        pq.push({0,{src,0}}); // initially src will be zero.
+        queue<pair<int,pair<int,int>>> q; // daiistance, node, k
+        q.push({0,{src,0}});
         vector<int> dist(n,1e9);
-        dist[src] = 0;
-        
-        int ans =  1e9;
-        while(!pq.empty()){
-                int dis = pq.top().second.second;
-                int level = pq.top().first;
-                int node = pq.top().second.first;
-                
-                pq.pop();
-                if(node==dst){ ans = min(ans,dis);continue;} 
-                if(level>k) continue;
-                
-                
-                for(auto it:adj[node]){
-                   
-                    
-                    if(level<=k && dist[it.first]>(dis+it.second)){
-                        dist[it.first] = dis+it.second;
-                        pq.push({level+1,{it.first,dist[it.first]}});
-                    }
+        while(!q.empty()){
+            int distance = q.front().first;
+            int node = q.front().second.first;
+            int level = q.front().second.second;
+            q.pop();
+            if(level>k)  continue;
+            for(auto it:adj[node]){
+                int d = it.second;
+                if(dist[it.first] > (d+distance)){
+                    dist[it.first] = d+distance;
+                    if(it.first!=dst)
+                        q.push({dist[it.first],{it.first,level+1}});
                 }
-            
+            }
         }
 
-        return ans==1e9?-1:ans;
-
+        return dist[dst]==1e9?-1:dist[dst];
     }
 };
