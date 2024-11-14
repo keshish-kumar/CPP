28295b0f04abd8c7f6d8886582f5a599eaf8ea18
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 12:47:31 +0530

    Time: 0 ms (100%), Space: 17.1 MB (90.78%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        vector<int> ans;
-        // inddegree vertices.
-        vector<int> indegree(n,0);
-        vector<vector<int > > adj(n);
-        for(int i=0;i<pre.size();i++){
+         // topological ordering
+         vector<vector<int>> adj(n);
+
+         vector<int> l(n,0);
+         for(int i=0;i<pre.size();i++){
             adj[pre[i][1]].push_back(pre[i][0]);
-            indegree[pre[i][0]]++;
-        }
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.push(i);
-        }
-        while(!q.empty()){
-            int node = q.front(); q.pop();
-            ans.push_back(node);
-            for(auto it:  adj[node]){
-                indegree[it]--;
-                if(indegree[it]==0)
-                q.push(it);
+            l[pre[i][0]]++;
+         }
+
+         queue<int> q;
+         for(int i=0;i<n;i++) if(l[i]==0) q.push(i);
+        vector<int> ans;
+         while(!q.empty()){
+            ans.push_back(q.front());
+            for(auto it:adj[q.front()]){
+                
+                l[it]--;
+                if(l[it]==0) q.push(it);
             }
-        }
-        if(ans.size()!=n) return {};
-        return ans;
+            q.pop();
+         }
+
+        return ans.size()==n?ans:vector<int>();
         
+         
     }
 };
