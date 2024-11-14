ff3398d3e510b24e9d40d9b77b6409e1574b7b82
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 16:00:20 +0530

    Time: 55 ms (15.27%), Space: 47.8 MB (18.08%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-
-    // O(n)
-    // priority_queu log(priority_queue size()) , log(26)
     int leastInterval(vector<char>& tasks, int n) {
-       vector<int> v(26,0);
-       for(int i=0;i<tasks.size();i++)
-        v[tasks[i]-'A']++;
-        sort(v.begin(),v.end()); // sort this
+        priority_queue<int> pq;
+        map<char,int> mp;
+        for(int i=0;i<tasks.size();i++) mp[tasks[i]]++;
+        for(auto it:mp) pq.push(it.second);
 
-        int maxfreq = v[25];
-        int gddhe = maxfreq-1;
-        int idleslot = n*gddhe;
+        int ans =0;
+        while(!pq.empty()){
+            int k = 0;
+            vector<int> temp;
+            while(!pq.empty()){
+                //int f =  pq.top();
+                if(pq.top()-1>0)
+                   {temp.push_back(pq.top()-1);}
+                pq.pop();
+               
+                k++;
+                if(k==(n+1)) break;
+            }
+            
+            for (int count : temp) {
+                pq.push(count);
+            }
 
-        for(int i=24;i>=0;i--){
-            idleslot -= min(v[i],gddhe);
+            // If the queue is empty, add only k (tasks processed), otherwise, add n+1 (full cycle)
+            ans += pq.empty() ? k : (n + 1);
         }
-        if(idleslot<=0) return tasks.size();
-        else return tasks.size()+idleslot;
+        return ans;
     }
 };
