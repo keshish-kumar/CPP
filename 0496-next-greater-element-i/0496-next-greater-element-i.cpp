d7af17a3e6c34e3f7e48128476c646cdf0ff2714
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 17:44:05 +0530

    Time: 3 ms (49.94%), Space: 12.3 MB (55.16%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
-        // Here we have to find next greater element of each element of nums1 in nums2
-        vector<int> ans(nums1.size());
+        vector<int> right;
         stack<int> st;
-        
-        vector<int> temp(nums2.size(),-1);
-        st.push(nums2[nums2.size()-1]);
-        for(int i=nums2.size()-2;i>=0;i--){
-            // Now we have to check for this elemnt in nums2 which is greater elemnt
-            // we will take stack for this and check again
-            if(nums2[i]<nums2[i+1]) {temp[i]=nums2[i+1]; st.push(temp[i]);}
-            else if(!st.empty()) {
-                while(!st.empty() && st.top()<nums2[i]){
-                    st.pop();
-                }
-                if(!st.empty()){
-                    temp[i]=st.top();
-                }
+        int n = nums2.size();
+        st.push(n-1);
+        right.push_back(n);
+        int i =n-2;
+        while(i>=0){
+            while(!st.empty() && nums2[st.top()]<nums2[i]){
+                st.pop();
             }
+            if(st.empty()) right.push_back(n);
+            else right.push_back(st.top());
+            st.push(i);
+            i--;
         }
-        
+        reverse(right.begin(),right.end());
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[nums2[i]]={right[i]};
+        }
+
+        vector<int> ans;
         for(int i=0;i<nums1.size();i++){
-            int j=0;
-            while(j<nums2.size() ){
-                if(nums1[i]==nums2[j]){
-                    ans[i] = temp[j];
-                    break;
-                }
-                j++;
-            }
+            int k = mp[nums1[i]];
+            if(k==n) k=-1;
+            else k = nums2[k];
+            ans.push_back(k);
         }
         return ans;
     }
