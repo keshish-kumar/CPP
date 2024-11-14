7b865519f4df37881dd8d07974e5d785d22047c2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 17:28:17 +0530

    Time: 44 ms (34.74%), Space: 94.5 MB (9.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& height) {
-         // we have to find the boundary index of bothh left and right till were whats its max indx it can reach +1;
-         int n = height.size();
-        vector<int> left;
-        left.push_back(-1);
+    vector<int> solveleft(vector<int>& heights){
         stack<int> st;
+        vector<int> left;
         st.push(0);
-        for(int i=1;i<n;i++){
-            while(!st.empty() && height[st.top()]>=height[i]){
+        left.push_back(-1);
+        int i=1;
+        while(i<heights.size()){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
                 st.pop();
             }
-            if(st.empty()) {left.push_back(-1);st.push(i);}
-            else{
-                left.push_back(st.top());
-                st.push(i);
-            }
+            if(st.empty()) left.push_back(-1);
+            else left.push_back(st.top());
+            st.push(i);
+            i++;
         }
 
+        return left;
+    }
+    vector<int> solveright(vector<int>& heights){
+        int n = heights.size();
+        stack<int> st;
         vector<int> right;
-        while(!st.empty()) st.pop();
         right.push_back(n);
         st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(!st.empty() && height[st.top()]>height[i]){
-                st.pop();
-            }
-            if(st.empty()){right.push_back(n);st.push(i);}
-            else{
-                right.push_back(st.top());
+        int i = n-2;
+        while(i>=0){
+                while(!st.empty() && heights[st.top()]>=heights[i]){
+                    st.pop();
+                }
+                if(st.empty()) right.push_back(n);
+                else right.push_back(st.top());
                 st.push(i);
-            }
+                i--;
         }
-
         reverse(right.begin(),right.end());
-
+        return right;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        
+        vector<int> left = solveleft(heights);
+        vector<int> right = solveright(heights);
         int ans =0;
-        for(int i=0;i<n;i++){
-            ans = max(ans,(right[i]-left[i]-1)*height[i]);
+        for(int i=0;i<heights.size();i++){
+            //cout<<right[i]<<" "<<endl;
+            ans = max(ans,abs(right[i]-left[i]-1)*heights[i]);
         }
         return ans;
     }
