85e122eecdefeb1495d3a343aaf1254f974abe84
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-14 17:11:06 +0530

    Time: 1 ms (81.3%), Space: 38.3 MB (87.3%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<int> pq;
-        map<char,int> mp;
-        for(int i=0;i<tasks.size();i++) mp[tasks[i]]++;
-        for(auto it:mp) pq.push(it.second);
-
-        int ans =0;
-        while(!pq.empty()){
-            int k = 0;
-            vector<int> temp;
-            while(!pq.empty()){
-                //int f =  pq.top();
-                if(pq.top()-1>0)
-                   {temp.push_back(pq.top()-1);}
-                pq.pop();
-               
-                k++;
-                if(k==(n+1)) break;
-            }
-            
-            for (int count : temp) {
-                pq.push(count);
-            }
-
-            // If the queue is empty, add only k (tasks processed), otherwise, add n+1 (full cycle)
-            ans += pq.empty() ? k : (n + 1);
+       
+       vector<int> v(26,0);
+        for(int i=0;i<tasks.size();i++){
+            v[tasks[i]-'A']++;
         }
-        return ans;
+        sort(v.begin(),v.end());
+        int t = v[25];
+        int gap = t-1;
+        int idleslot  = (t-1)*n;
+
+        for(int i=24;i>=0;i--){
+            idleslot -= min(gap,v[i]);
+        }
+
+        if(idleslot>0) return tasks.size()+idleslot;
+        return  tasks.size();
+       
     }
 };
