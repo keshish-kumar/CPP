eb7e98f725d968f5e12fa447ca6c2323ebd732b5
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-17 13:22:57 +0530

    Time: 0 ms (100%), Space: 7.9 MB (90.77%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-     void comb(vector<int>& candidates,int target, vector<int> temp, vector<vector<int>>& ans, int n,int k){
-        if(target==0){
-            if(temp.size()==k)
-              ans.push_back(temp);
+    void solve(vector<int>& t, int index, int k, int n, vector<int>& temp, vector<vector<int>>& ans){
+        if(n==0 && temp.size()==k){
+            ans.push_back(temp);
             return;
         }
-        if(n==candidates.size()) return;
-        
-       
-        for(int i=n;i<candidates.size();i++){
-            if(i>n && candidates[i]==candidates[i-1]) continue;
-            
-            if(candidates[i]<=target){
-                temp.push_back(candidates[i]);
-                comb(candidates,target-candidates[i],temp,ans,i+1,k);
-                temp.pop_back(); // this should not be included in next combination as its already used here.
+
+        for(int i=index;i<t.size();i++){
+            if(n>=t[i]){
+                temp.push_back(t[i]);
+                solve(t,i+1,k,n-t[i],temp,ans);
+                temp.pop_back();
             }
         }
-        
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> candidates;
-        for(int i=0;i<9;i++){
-            candidates.push_back(i+1);
-        }
-         vector<vector<int>> ans;
+        vector<int> t = {1,2,3,4,5,6,7,8,9};
+        vector<vector<int>> ans;
         vector<int> temp;
-        sort(candidates.begin(),candidates.end());
-        comb(candidates,n,temp,ans,0,k);
+        solve(t,0,k,n,temp,ans);
         return ans;
     }
 };
