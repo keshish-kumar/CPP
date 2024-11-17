a156d3e6abbfa11f084740de1f8770a12df05c4e
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-17 15:21:28 +0530

    Time: 2869 ms (5.03%), Space: 499.9 MB (5.01%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    bool solve(int n, int m, vector<vector<char>>& board, string & word,int p,vector<vector<int>>& visited){
-        if(p>=word.size()) return true;
-        vector<int> ii={0,1,0,-1};
-        vector<int> jj={1,0,-1,0};
-       
-       
-       visited[n][m] = 1;
-                for(int k=0;k<4;k++){
-                    int row = n+ii[k];
-                    int col = m+jj[k];
-                    if(row>=0 && col>=0 && row<board.size() && col<board[0].size() && visited[row][col]==0 && board[row][col]==word[p]){             
-                        if(solve(row,col,board,word,p+1,visited)) return true;
-                         
-                    }
-                }
-               visited[n][m] = 0;
 
-                return false;
+    bool solve(vector<vector<char>>& board, string & word, int i, int j, int index,vector<vector<int>>& visited){
+        if(index==word.size()) return true;
+        vector<int> ii = {1,0,-1,0};
+        vector<int> jj = {0,1,0,-1};
+        for(int k=0;k<4;k++){
+            int r= i+ii[k];
+            int c = j+jj[k];
+            if(r>=0 && c>=0 && r<board.size() && c<board[0].size()&& visited[r][c]==-1 && board[r][c] == word[index]){
+                visited[r][c] = 1;
+                if(solve(board,word,r,c,index+1,visited)) return true;
+                visited[r][c] = -1;
+            }
+        }
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        
-        vector<vector<int>> visited(board.size(),vector<int>(board[0].size(),0));
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-               
-                if(board[i][j] == word[0] &&  visited[i][j] == 0){
-                     visited[i][j] = 1;
-                    if(solve(i,j,board,word,1,visited)) return true;
-                     visited[i][j] = 0;
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == word[0]){
+                    vector<vector<int>> visited(n,vector<int>(m,-1));
+                    visited[i][j] = 1;
+                    if(solve(board,word,i,j,1,visited)) return true;
                 }
             }
         }
