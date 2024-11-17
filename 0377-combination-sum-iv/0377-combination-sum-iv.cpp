e4fa346224a8778b48d891f15ffcddd8df1a40da
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-17 15:03:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (53.2%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int target, int & ans, int index,vector<vector<int>>& dp){
+    int solve(vector<int>& nums, int target,int index,vector<int>& dp){
         if(target == 0){
-            ans++;
             return 1;
         }
         if(index==nums.size() || target<0) return 0;
-        if(dp[index][target]!=-1) return dp[index][target];
-
-        int l = 0;
-        if(target>=nums[index]){
-            l = solve(nums,target-nums[index],ans,0,dp);
-            
-        }
-        int r = 0;
-        r = solve(nums,target,ans,index+1,dp);
-        return dp[index][target] = l+r;
+        if(dp[target]!=-1) return dp[target];
+       int ans = 0;
+       for(int i=index;i<nums.size();i++){
+         int l = solve(nums,target-nums[i],0,dp);
+            ans += l;
+       }
+       return dp[target] = ans;
 
     }
     int combinationSum4(vector<int>& nums, int target) {
-        
-        int ans =0;
-
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-        return solve(nums, target, ans,0,dp);
-        return ans;
+        vector<int> dp(target+1,-1);
+        return solve(nums, target,0,dp);
 
     }
 };
