01077dc19e814dd9c4f6b62618ff4bf310ce4b07
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-22 18:23:47 +0530

    Time: 0 ms (100%), Space: 14.9 MB (48.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,23 +2,31 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        for(int i=nums.size()-2;i>=0 ;i--){
-            int left = 1e9;
-            for(int j = nums.size()-1;j>i;j--){
-                if(nums[j]>nums[i]){
-                    if(left==1e9) left = j;
-                    else{
-                        if(nums[j]<nums[left]) left = j;
+        // first we have to find the element which statiosfys arr[i-1]<arr[i] from right nindex 
+        // than after getting the index (i-1) we have to ssap with element on right side of it which is just greater that that.
+        // than we have to revsre from i to end;
+        // else if everything is in descedning order than we ahve to ervsre it
+        for(int i=nums.size()-1;i>0;i--){
+            // wehave to look for the cse arr[i-1]<arr[i]
+            if(nums[i-1]<nums[i]){
+                
+                // Now we have to search for the element which is just graeter than arr[i-1]
+                int k = 0;
+                for(int j=nums.size()-1;j>=i;j--){
+                    if(nums[j]>nums[i-1]){
+                       
+                        k=j;
+                        break;
                     }
                 }
-            }
-            if(left!=1e9){
-            swap(nums[i],nums[left]);
-            reverse(nums.begin()+i+1,nums.end());
-            return;
+                swap(nums[i-1],nums[k]);
+                reverse(nums.begin()+i,nums.end());
+                return ;
+
             }
         }
-       
         reverse(nums.begin(),nums.end());
+
+        
     }
 };
