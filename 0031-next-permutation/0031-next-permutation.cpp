4653a3536b5f8ce7f648568855acd1438aaae530
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-25 22:31:33 +0530

    Time: 0 ms (100%), Space: 14.9 MB (5.67%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
-        // first we have to find the element which statiosfys arr[i-1]<arr[i] from right nindex 
-        // than after getting the index (i-1) we have to ssap with element on right side of it which is just greater that that.
-        // than we have to revsre from i to end;
-        // else if everything is in descedning order than we ahve to ervsre it
+        //1->find the lement such that nums[i-1]<nums[i];
+        //2-> find the lement which is just grewater than the lement nums[j-1] from right 
+        // 3->swap fromt the two and reverse from index i to end of array
+
+        int n = nums.size();
         for(int i=nums.size()-1;i>0;i--){
-            // wehave to look for the cse arr[i-1]<arr[i]
             if(nums[i-1]<nums[i]){
-                
-                // Now we have to search for the element which is just graeter than arr[i-1]
-                int k = 0;
-                for(int j=nums.size()-1;j>=i;j--){
+                // we have found the pivot elemnt
+                // Now look for element which is just greater tha that
+                for(int j=nums.size()-1;j>=i ;j--){
                     if(nums[j]>nums[i-1]){
-                       
-                        k=j;
+                        swap(nums[j],nums[i-1]);
                         break;
                     }
                 }
-                swap(nums[i-1],nums[k]);
                 reverse(nums.begin()+i,nums.end());
-                return ;
-
+                return;
             }
         }
         reverse(nums.begin(),nums.end());
-
-        
     }
 };
