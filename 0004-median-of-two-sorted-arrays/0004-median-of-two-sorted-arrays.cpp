c5f02240de4cbdffc8fddfd9b7115ea1baa41141
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-25 22:21:07 +0530

    Time: 4 ms (25.73%), Space: 94.6 MB (93.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        priority_queue<int> left;
-        priority_queue<int,vector<int>,greater<int>> right;
-        int i=0,j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                right.push(nums1[i]);
-                i++;
-            }
-            else{
-                right.push(nums2[j]);
-                j++;
-            }
-
-            if((right.size()-left.size()) >1){
-                left.push(right.top());
-                right.pop();
-            }
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
         }
+        int low = 0,high = nums1.size();
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        while(low<=high){
+           int px = (low+high)/2; // element which will be from num1 in left slot
+           int py = (n+m+1)/2-px; // element which will be from num2 in right slot
 
-        while(i<nums1.size()){
-            right.push(nums1[i]);
-            if((right.size()-left.size())>1){
-                left.push(right.top());
-                right.pop();
-            }
-            i++;
-        }
+           // first half
+           int x1 = (px==0)?-1e9:nums1[px-1];
+           int x2 = (py==0)?-1e9:nums2[py-1];
 
-        while(j<nums2.size()){
-            right.push(nums2[j]);
-            if((right.size()-left.size())>1){
-                left.push(right.top());
-                right.pop();
-            }
-         j++;
+           // right half
+           int x3 = (px==n)?1e9:nums1[px];
+           int x4 = (py==m)?1e9:nums2[py];
+
+           if(x1<=x4 && x2<=x3){
+            if((n+m)%2==0) return (max(x1,x2)+min(x3,x4))/2.0;
+            else return max(x1,x2);
+           }
+
+           if(x1>x4){
+            high = px-1;
+           }
+           else
+            low = px+1;
+
         }
 
-        if((right.size()-left.size())==1){
-            return right.top();
-        }
-        else{
-            return (right.top()+left.top())/2.00;
-        }
+        return 0;
     }
 };
