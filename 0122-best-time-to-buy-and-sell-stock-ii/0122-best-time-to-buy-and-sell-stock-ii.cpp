4d76d2d2d72958e695d2cb353adb4068e74f6e02
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-26 15:19:14 +0530

    Time: 3 ms (30.96%), Space: 22.1 MB (6.35%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int i, int buy,vector<vector<int>>& dp){
-        if(i==prices.size()) return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy){
-            return dp[i][buy] = max(-prices[i]+solve(prices,i+1,!buy,dp),solve(prices,i+1,buy,dp));
+    int solve(vector<int>& prices, int buy, int index,vector<vector<int>>& dp){
+        if(index>=prices.size()) return 0;
+        if(dp[index][buy]!=-1) return dp[index][buy];
+        if(!buy){
+            return dp[index][buy] = max(-prices[index]+solve(prices,!buy,index+1,dp),solve(prices,buy,index+1,dp));
         }
         else{
-            return dp[i][buy] = max(prices[i]+solve(prices,i+1,!buy,dp),solve(prices,i+1,buy,dp));
+            return dp[index][buy] = max(prices[index]+solve(prices,!buy,index+1,dp),solve(prices,buy,index+1,dp));
         }
     }
     int maxProfit(vector<int>& prices) {
-        // if w bought than we cannot buy firuther we have to sell 
-        // simillalry if we solbde we hvae to buy first
+        
+        int buy = 0;
         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return solve(prices,0,1,dp);
+        return solve(prices, buy, 0,dp);
     }
 };
