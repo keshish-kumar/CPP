1f2050cb9e4e7822c87ff58b44abb01f287ed2bb
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-26 15:04:21 +0530

    Time: 177 ms (75.28%), Space: 184.2 MB (27.38%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int timer= 1;
     vector<vector<int>> ans;
-    void dfs(int node, int parent, vector<int>& low, vector<int>& time, vector<vector<int>>& adj,vector<int>& visited){
+    int timer = 1;
+
+    void dfs(int node, int parent , vector<int>& low, vector<int>& time, vector<vector<int>>& adj, vector<int>& visited){
         visited[node] = 1;
-        time[node]=low[node]=timer;
+        time[node] = low[node]=timer;
         timer++;
         for(auto it:adj[node]){
-            if(it == parent) continue;
+            if(it == parent ) continue;
             if(visited[it]==0){
                 dfs(it,node,low,time,adj,visited);
                 low[node] = min(low[node],low[it]);
@@ -16,25 +17,27 @@ public:
                 }
             }
             else{
-                low[node] = min(time[it],low[node]);
+                low[node] = min(low[node],low[it]);
             }
+
         }
     }
+
+
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         
-        vector<int> low(n,0);
-        vector<int> time(n,0);
-        vector<int> visited(n,0);
-
         vector<vector<int>> adj(n);
         for(int i=0;i<connections.size();i++){
             adj[connections[i][0]].push_back(connections[i][1]);
             adj[connections[i][1]].push_back(connections[i][0]);
         }
 
-       dfs(0,-1,low,time,adj,visited);
-       return ans;
+        vector<int> low(n,0);
+        vector<int> time(n,0);
+        vector<int> visited(n,0);
 
+        dfs(0,-1,low,time,adj,visited);
+        return ans;
 
     }
 };
