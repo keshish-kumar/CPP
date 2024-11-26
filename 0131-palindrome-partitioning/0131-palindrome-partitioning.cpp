42d1385c555a729588448ad1b4e8a7a62586cfe5
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-11-26 13:24:18 +0530

    Time: 56 ms (68.62%), Space: 79.7 MB (28.41%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    bool permut(int start, int end, string s){
-        if(end==start) return true;
-        while(start<end){
-            if(s[start++]!=s[end--]) return false;
-
+    vector<vector<string>> ans;
+    vector<string> temp;
+    bool ispossible(string & s, int i, int j){
+        
+        while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;j--;
         }
         return true;
     }
-    void solve(string& s ,int n,vector<vector<string>>& ans, vector<string>& temp){
-        if(n>=s.size()){
+    void solve(string & s, int i){
+        if(i>=s.size()){
             ans.push_back(temp);
+            
         }
-        for(int i=n;i<s.size();i++){
-            if(permut(n,i,s)){
-                temp.push_back(s.substr(n,(i-n+1)));
-                // cout<<temp<<endl;
-                solve(s,i+1,ans,temp);
+
+        for(int j=i;j<s.size();j++){
+            if(ispossible(s,i,j)){
+                temp.push_back(s.substr(i,j-i+1));
+                solve(s,j+1);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        // first we have to make one division than we have to make another division
-        vector<vector<string>> ans;
-        vector<string> temp;
-        solve( s, 0,ans,temp);
-        return ans;
         
+        solve(s,0);
+        return ans;
     }
 };
