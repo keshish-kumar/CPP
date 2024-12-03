fb9f3b92c0afc3865b32a58934c2a2cb8c6fb3a2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-12-03 19:03:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Palindrome/palindrome.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+	    
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int reverse = 0;
+		    int temp=n;
+		    while(temp!=0){
+		        reverse = reverse*10 + (temp%10);
+		        temp = temp/10;
+		    }
+		    
+		    if(reverse == n) return "Yes";
+		    else return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    
+cout << "~" << "\n";
+}
+	return 0;
+}
+
+// } Driver Code Ends
