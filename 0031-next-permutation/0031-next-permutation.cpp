e57ca85c22c5032acc053ad09f5895f429a10a6d
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2024-12-24 19:16:15 +0530

    Time: 0 ms (100%), Space: 15.8 MB (14.9%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //1->find the lement such that nums[i-1]<nums[i];
-        //2-> find the lement which is just grewater than the lement nums[j-1] from right 
-        // 3->swap fromt the two and reverse from index i to end of array
-
-        int n = nums.size();
-        for(int i=nums.size()-1;i>0;i--){
+         
+         // first we have to find about the pivot element
+         // element fom right is. statosys arr[i-1]<arr[i]
+         for(int i=nums.size()-1;i>0;i--){
             if(nums[i-1]<nums[i]){
-                // we have found the pivot elemnt
-                // Now look for element which is just greater tha that
-                for(int j=nums.size()-1;j>=i ;j--){
+                for(int j=nums.size()-1;j>=i;j--){
                     if(nums[j]>nums[i-1]){
-                        swap(nums[j],nums[i-1]);
+                        swap(nums[i-1],nums[j]);
                         break;
                     }
                 }
                 reverse(nums.begin()+i,nums.end());
                 return;
             }
-        }
-        reverse(nums.begin(),nums.end());
+         }
+         reverse(nums.begin(),nums.end());
     }
 };
