b456a7be92dfbac40448f644e1f6ee9adf2c23cf
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-02-04 20:59:15 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        // Here we are given sorted repeated array and we need to find th first and last position of occurance of that element
-
+    int lowerbound(vector<int>& nums, int target){
         int low=0,high=nums.size()-1;
-        int ans1=-1,ans2=-1;
+        int ans =-1;
         while(low<=high){
             int mid = (low+high)/2;
 
             if(nums[mid]==target){
-                int i=mid,j=mid;
-                while(i>=0 && nums[i]==target){
-                    ans1 = i;
-                    i--;
-                }
-                while(j<nums.size() && nums[j]==target){
-                    ans2=j;
-                    j++;
-                }
-                return {ans1,ans2};
+                ans=mid;
             }
-
-            if(nums[mid]<target){
+            if(nums[mid] >= target){
+                high = mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    int upperbound(vector<int>&nums,int target){
+        int low=0,high=nums.size()-1;
+        int ans=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans=mid;
+            }
+            if(nums[mid]<= target){
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        return {ans1,ans2};
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+
+        return {lowerbound(nums,target),upperbound(nums,target)};
     }
 };
