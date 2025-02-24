de33964814db78e35bcabd2e22caf7d2ece2b82c
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-02-24 12:10:34 +0530

    Time: 2 ms (20.61%), Space: 12.1 MB (10.38%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int> mp;
+        unordered_map<int,int> mp;
 
         for(int i=0;i<arr.size();i++){
             mp[arr[i]]++;
         }
         // Now we need to check if same number exist or not
-        set<int> ans; 
+        unordered_set<int> ans; 
         for(auto it:mp){
             ans.insert(it.second);
         }
