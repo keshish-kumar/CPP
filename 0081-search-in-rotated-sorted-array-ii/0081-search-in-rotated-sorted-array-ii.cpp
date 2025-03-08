4fe483d843457cff1bf629dc493d796643bd67b3
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-08 17:01:38 +0530

    Time: 0 ms (100%), Space: 18 MB (20.58%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-
-        int low = 0, high = nums.size() - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target)
-                return true;
-            if(nums[low]==nums[mid] && nums[mid]==nums[high]) {low++;high--;}
-            else if (nums[low] <= nums[mid]) { // left hafl is sorted
-                if (nums[low] <= target && nums[mid] >= target) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            // cout<<low<<" "<<mid<<" "<<high<<endl;
+            if(nums[mid]==target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){low++;high--;}
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
                 }
-            } else {
-                if (nums[mid] <= target && nums[high] >= target) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
                 }
             }
         }
