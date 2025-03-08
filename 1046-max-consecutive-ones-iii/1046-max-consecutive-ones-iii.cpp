9afc9986492614eef01448a1b5dfe9a2663716c1
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-09 02:30:41 +0530

    Time: 1881 ms (5.1%), Space: 59.3 MB (59.18%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int i=0,j=0;
-        int ans = 0;
-        int count =0;
-        int t = k;
+        // as it is given we can only flip 0's
+        // we will run sliding winow in this.
+        int i=0,j=0,temp=k,ans=0;
         while(j<nums.size()){
-            if(nums[j]==1 || t--) count++;
+            
+          
+            if(nums[j]==0 ){
+            if(temp>0){
+                temp--;
+            }
             else {
-                while(nums[i]!=0){
+                while(i<j && nums[i]!=0){
                     i++;
-                    count--;
                 }
                 i++;
-                t=0;
                 
             }
-            ans = max(ans,count);
+        }
+        ans = max(ans,(j-i+1));
+        cout<<i<<" "<<j<<endl;
             j++;
         }
         return ans;
