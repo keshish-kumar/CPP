59f3eaf697a6c2964eff3dcf3fb57377660dd0db
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-09 02:08:06 +0530

    Time: 338 ms (7.49%), Space: 12.5 MB (42.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        set<char> st;
-        int i=0,j=0;
-        int ans =0;
+        // lets try using sliding window
+        int i=0,j=0,ans=0;
+
+        unordered_map<char,int> mp;
         while(j<s.size()){
-            
-            while(!st.empty() && st.find(s[j])!=st.end()){
-                st.erase(s[i]);
+            cout<<i<<" "<<j<<endl;
+            if(mp.find(s[j])==mp.end()){
+                mp[s[j]]++;
+            }
+            else{
+                while(i<j && s[i]!=s[j]){
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0) mp.erase(s[i]);
+                    i++;
+                }
                 i++;
             }
-
-            st.insert(s[j]);
-
-            ans = max ( ans, (j-i+1));
+            ans = max(ans,(int)mp.size());
             j++;
-
         }
         return ans;
     }
