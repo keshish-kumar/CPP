a03e27320e1c6b95d920214899b3880dc59063e5
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-09 19:27:20 +0530

    Time: 0 ms (100%), Space: 17.8 MB (57.25%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,15 +4,16 @@ public:
         int ans = -1e9;
         int prod = 1;
         for(int i=0;i<nums.size();i++){
-            prod *=nums[i];
-            ans = max(ans,prod);
-            ans = max(ans,nums[i]);
-        }
-        prod = 1;
-        for(int i=nums.size()-1;i>=0 ; i--){
             prod *= nums[i];
             ans = max(ans,prod);
-            ans = max(ans,nums[i]);
+            if(prod==0) prod = 1;
+        }
+
+        prod = 1;
+        for(int i=nums.size()-1;i>=0;i--){
+            prod *= nums[i];
+            ans = max(ans,prod);
+            if(prod==0) prod = 1;
         }
         return ans;
     }
