1c08d91928e5bbeb94758be5770886e50c5ad84d
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-24 17:33:34 +0530

    Time: 0 ms (100%), Space: 11.8 MB (8.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int i=0,j=0,k=nums.size()-1; // i is for 0, j is for 1 and k is for 2
-
+        // first approch is we keep the count of 1's 09's and 2's and modify the array accordingly
+        // second method using three pointers
+        int i=0,j=0,k=nums.size()-1; // i-0,j-1,k-2
         while(k>=j){
             if(nums[j]==1) j++;
-           else if(nums[j]==2){
+            else if(nums[j]==2){
                 swap(nums[j],nums[k]);
                 k--;
-           }
-           else{ // nums[j]=0
-            swap(nums[i],nums[j]);
-            i++;j++;
-           }
+            }
+            else{ // nums[j]=0
+                swap(nums[i],nums[j]);
+                i++;j++;
+            }
         }
+        //return;
     }
 };
