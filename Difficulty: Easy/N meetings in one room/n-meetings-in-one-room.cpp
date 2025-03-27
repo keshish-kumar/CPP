23b0b9a685281c52bee236c8883b5f644f3575d2
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-27 17:22:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -4,49 +4,71 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
-    int maxMeetings(int n, int start[], int end[]) {
+    static bool compr(pair<int,int> a, pair<int,int> b){
+        if(a.second == b.second){
+            return a.first < b.first;
+        }
+        return a.second<b.second;
+    }
+    int maxMeetings(vector<int>& start, vector<int>& end) {
         // Your code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q; 
-        for(int i=0;i<n;i++){
-            q.push({end[i],start[i]});
+        vector<pair<int,int>> v;
+        for(int i=0;i<start.size();i++){
+            v.push_back({start[i],end[i]});
         }
         
-        int str = 0,ed=0,ans=0;
+        sort(v.begin(),v.end(),compr);
+        // Now we will make one stack and solve this
+        // the size of maium stack will be its answer.
         
-        while(!q.empty()){
-            pair<int,int> p = q.top();q.pop();
-            if(p.second > ed){
+        int ans = 0;
+        int endtime = -1;
+        for(int i=0;i<v.size();i++){
+            if(v[i].first > endtime){
                 ans++;
-                str=p.second;
-                ed = p.first;
+                endtime = v[i].second;
             }
+            
         }
-        
         return ans;
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        int start[n], end[n];
-        for (int i = 0; i < n; i++)
-            cin >> start[i];
+        string input;
+        int num;
+        vector<int> start;
 
-        for (int i = 0; i < n; i++)
-            cin >> end[i];
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            start.push_back(num);
+        }
+
+        vector<int> end;
+        getline(cin, input);
+        stringstream s22(input);
+        while (s22 >> num) {
+            end.push_back(num);
+        }
 
         Solution ob;
-        int ans = ob.maxMeetings(n, start, end);
+        int ans = ob.maxMeetings(start, end);
         cout << ans << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
