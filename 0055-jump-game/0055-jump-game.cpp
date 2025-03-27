8779cee4bfaf952fc3d73ddf7e9d5b4531f72c30
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-03-27 17:55:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int index,vector<int>& dp){
-        if(index>=(nums.size()-1)){
-            return true;
+    bool solve(vector<int>& nums, int i,vector<int>& dp){
+        if(i>=(nums.size()-1))return true;
+        if(dp[i]!=-1) return dp[i];
+        bool ans = false;
+        for(int j = 1;j<=nums[i];j++){
+           ans = ans || solve(nums,i+j,dp);
         }
-        if(dp[index]!=-1) return dp[index];
-         bool flag = false;
-        for(int i=1;i<=nums[index];i++){
-            flag = flag||solve(nums,index+i,dp);
-        }
-
-        return dp[index]=flag;
+        return dp[i]=ans;
     }
     bool canJump(vector<int>& nums) {
-        // vector<int> dp(nums.size(),-1);
-        // return solve(nums,0,dp);
-
-        // Now we do using tabulation approach.
-        
-        int maxi=0;
-        for(int i=0;i<nums.size();i++){
-            if(i>maxi) return false; // it means that this index i cannot be reached fron anwere before popints
-            maxi = max(maxi,i+nums[i]);
-        }
-        return true;
+        vector<int> dp(nums.size(),-1);
+        return solve(nums,0,dp);
     }
 };
