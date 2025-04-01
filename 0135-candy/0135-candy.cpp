d76bd9f49c98be6d0efb79d8a5cf3d0d242d1d2a
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-04-01 18:15:54 +0530

    Time: 0 ms (100%), Space: 23.9 MB (6.83%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n,1);
-        vector<int> right(n,1);
-
+        int ans = 0;
+        vector<int> countLeft(n,1);
         for(int i=1;i<n;i++){
-            if(ratings[i-1]<ratings[i]){
-                left[i]=left[i-1]+1;
+            if(ratings[i]>ratings[i-1]){
+                countLeft[i]=countLeft[i-1]+1;  
             }
         }
-
+       vector<int> countRight(n,1);
         for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+            if(ratings[i]>ratings[i+1] ){
+                countRight[i] = countRight[i+1]+1;
             }
         }
-
-        int ans=0;
+        vector<int> count(n,0);
         for(int i=0;i<n;i++)
-            ans += max(left[i],right[i]);
+           { count[i] = max(countLeft[i],countRight[i]);
+               
+           }
+           
+        ans = accumulate(count.begin(),count.end(),0);
+
         return ans;
     }
 };
