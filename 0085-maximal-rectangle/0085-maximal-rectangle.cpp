21c8309654ccd74f79c8e25ae73ec1a6cd6915b8
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-04-02 21:47:04 +0530

    Time: 11 ms (41.46%), Space: 23.5 MB (8.97%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,64 +1,65 @@
 class Solution {
 public:
-    vector<int> leftvertex(vector<int>& temp){
-        vector<int> ans;
+    vector<int> solveLeft(vector<int>& heights){
+        vector<int> left;
         stack<int> st;
-        st.push(0);
-        ans.push_back(-1);
-        for(int i=1;i<temp.size();i++){
 
-            while(!st.empty() && temp[st.top()]>=temp[i]){
-                st.pop();
+        for(int i=0;i<heights.size();i++){
+            if(st.empty()){
+                left.push_back(-1);
             }
-            if(st.empty()) ans.push_back(-1);
-            else ans.push_back(st.top());
+            else{
+                while(!st.empty() && heights[st.top()] >= heights[i])
+                    st.pop();
+                if(!st.empty()) left.push_back(st.top());
+                else left.push_back(-1);
+            }
             st.push(i);
         }
-
-        return ans;
+        return left;
     }
-
-    vector<int> rightvertex(vector<int>& temp){
-        int n= temp.size();
-        vector<int> ans;
+     vector<int> solveRight(vector<int>& heights){
+        vector<int> right;
         stack<int> st;
-        st.push(n-1);
-        ans.push_back(n);
-        for(int i=n-2;i>=0;i--){
-            while(!st.empty() && temp[st.top()]>temp[i]){
-                st.pop();
+        int n = heights.size();
+        for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                right.push_back(n);
             }
-            if(st.empty()) ans.push_back(n);
-            else ans.push_back(st.top());
+            else{
+                while(!st.empty() && heights[st.top()] > heights[i])
+                    st.pop();
+                if(!st.empty()) right.push_back(st.top());
+                else right.push_back(n);
+            }
             st.push(i);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(right.begin(),right.end());
+        return right;
     }
-
-    int solve(vector<int>& temp){
-        vector<int> left = leftvertex(temp);
-        vector<int> right = rightvertex(temp);
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int> left = solveLeft(heights);
+        vector<int> right = solveRight(heights);
         int ans =0;
-        for(int i=0;i<temp.size();i++){
-            int t = right[i]-left[i]-1;
-            ans = max(ans,temp[i]*t);
+        for(int i=0;i<heights.size();i++){
+            ans = max(ans, (right[i]-left[i]-1)*heights[i]);
         }
         return ans;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        // Here we need to find maimum area in rectangle
-        if (matrix.empty() || matrix[0].empty()) return 0;
-        vector<int> temp(matrix[0].size(),0);
+        int n = matrix[0].size();
+        vector<int> heights(n,0);
         int ans =0;
         for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]=='0') temp[j]=0;
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]=='0'){
+                    heights[j] = 0;
+                }
                 else{
-                    temp[j] += 1;
+                    heights[j] += 1;
                 }
             }
-            ans = max(ans,solve(temp));
+            ans = max(ans,largestRectangleArea(heights));
         }
         return ans;
     }
