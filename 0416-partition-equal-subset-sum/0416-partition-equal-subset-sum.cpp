ef67e56ddf28ad463d26267a3bf0eb44b059592c
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-04-05 11:34:20 +0530

    Time: 55 ms (90.05%), Space: 100.2 MB (45.24%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int i, int sum,int acc,vector<vector<int>>& dp){
-        if(i>=nums.size()) return false;
-
-        if(sum-acc == acc) return true;
-        if(dp[i][acc]!=-1) return dp[i][acc];
-        //pick
-        return dp[i][acc] =  solve(nums,i+1,sum,acc+nums[i],dp) || solve(nums,i+1,sum,acc,dp);
-
+    bool solve(vector<int>& nums, int target, int i,vector<vector<int>>& dp){
+        if(target == 0) return true;
+        if(target<0 || i>=nums.size()) return false;
+        if(dp[i][target]!=-1) return dp[i][target];
+        dp[i][target] = solve(nums,target-nums[i],i+1,dp) || solve(nums,target,i+1,dp);
+        return dp[i][target];
     }
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for(int i=0;i<nums.size();i++) sum += nums[i];
-        //vector<vector< int >> dp(nums.size(),vector<int>(sum,-1));
-        //return solve(nums,0,sum,0,dp);
+        //. equal partititon can onky be performed when sum is even number odd number cannot be partriitoned equally
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!=0) return false;
 
-        // we will use direclty sapce optimized approach
-        if (sum % 2 != 0) return false;
-        sum = sum/2;
-        vector<bool> dp(sum+1,false);
-        dp[0] = true;
-        for(int i=1;i<=nums.size();i++){
-            vector<bool> temp(sum+1,false);
-            temp[0] = true;
-            for(int j=1;j<=sum;j++){
-               // if(sum-j == j) return true;
-                bool pick = dp[j];
-                bool notpick = false;
-                if(j>=nums[i-1])
-                    notpick = dp[j-nums[i-1]];
-                temp[j] = pick||notpick;
-            }
-            dp = temp;
-        }
-
-        return dp[sum];
+        int target = sum/2;
+        vector<vector<int> > dp(nums.size(),vector<int>(target+1,-1));
+        return solve(nums,target,0,dp);
+        
     }
 };
