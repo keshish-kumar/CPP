e5b08a2b71c43ce2f453152d4ce65d62b7be7e0f
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-04-06 19:09:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -6,46 +6,52 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 // User function Template for C++
 
-class Solution{
+class Solution {
   public:
-  int solve(int price[], int n, int w,vector<vector<int>>& dp){
-      if(n==0 || w==0) return 0;
-      if(dp[n][w]!=-1) return dp[n][w];
-      int l = solve(price,n-1,w,dp);
-      int r = 0;
-      if(w>=n){
-          r = price[n-1]+solve(price,n,w-n,dp);
-      }
-      
-      return dp[n][w]=max(l,r);
-  }
-    int cutRod(int price[], int n) {
-        //code here
+    int solve(vector<int>& price, int n, int i,vector<vector<int>>& dp){
+        
+        if(i==0 || n==0) return 0;
+        if(dp[i][n]!=-1) return dp[i][n];
+        int notpick = solve(price,n,i-1,dp);
+        int pick = 0;
+        if(i<=n){
+            pick = price[i-1] + solve(price,n-i,i,dp);
+        }
+        return dp[i][n] = max(pick,notpick);
+    }
+    int cutRod(vector<int> &price) {
+        // code here
+        int n = price.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
         return solve(price,n,n,dp);
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
     int t;
-    cin >> t;
+    scanf("%d ", &t);
     while (t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++) 
-            cin >> a[i];
-            
+
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
         Solution ob;
 
-        cout << ob.cutRod(a, n) << endl;
-    
-cout << "~" << "\n";
-}
+        cout << ob.cutRod(a) << endl;
+        cout << "~" << endl;
+    }
     return 0;
 }
 // } Driver Code Ends
