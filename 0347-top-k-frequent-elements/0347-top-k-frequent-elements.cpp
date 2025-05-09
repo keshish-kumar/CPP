f587b4b5bedd3446ee55afd1b090632ec1df93bc
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-09 23:11:01 +0530

    Time: 0 ms (100%), Space: 17.8 MB (91.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // as we need to find most frequent element we will keep this i pair 
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        // Now elements wiht its frequency is stored.
-        // top k frequent element
-        priority_queue<pair<int,int>> pq;
-        for(auto it: mp){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto it:mp){
             pq.push({it.second,it.first});
+            if(pq.size()>k) pq.pop();
         }
         vector<int> ans;
-        while(k--){
-            ans.push_back({pq.top().second});
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
             pq.pop();
         }
-
         return ans;
     }
 };
