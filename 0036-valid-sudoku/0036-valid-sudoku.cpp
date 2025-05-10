fa8b98451eacc0deeab67abbf23a129615d959a3
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-10 17:41:55 +0530

    Time: 0 ms (100%), Space: 21.5 MB (96.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-            for(int i=0;i<9;i++){
-                for(int j=0;j<9;j++){
-                    if(board[i][j]>='1' && board[i][j]<='9'){
-                        for(int k=0;k<9;k++){
-                            if(board[i][k]==board[i][j] && k!=j) return false;
-                            if(board[k][j]==board[i][j] && k!=i) return false;
-                            if(board[3*(i/3)+k/3][3*(j/3)+k%3]==board[i][j] &&i!=3*(i/3)+k/3 && j!=3*(j/3)+k%3 ) return false;
-                        }
-
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]>='1' && board[i][j]<='9'){
+                    for(int k=0;k<9;k++){
+                        if(board[i][k]==board[i][j] && j!=k ) return false;
+                        if(board[k][j]==board[i][j] && i!=k ) return false;
+                        if(board[3*(i/3)+k/3][3*(j/3)+k%3] == board[i][j] && i!=(3*(i/3)+k/3) && j != (3*(j/3)+k%3)) return false;
                     }
                 }
             }
-            return true;
+        }
+        return true;
     }
 };
