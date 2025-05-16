174bbf382a214921d6f521317da7808a090ce941
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-16 15:08:14 +0530

    Time: 7 ms (8.26%), Space: 63 MB (12.67%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,54 +1,15 @@
 class Solution {
 public:
-    void SolveLeft(vector<int>& height, vector<int>& left){
-        stack<int> st;
-
-        for(int i=0;i<height.size();i++){
-            if(st.empty()){
-                st.push(i);
-                left.push_back(i);
-            }
-            else{
-                while(!st.empty() && height[st.top()] < height[i]){
-                    st.pop();
-                }
-                if(!st.empty()) left.push_back(st.top());
-                else{ left.push_back(0);} 
-                st.push(i);
-            }
-            
-        }
-
-    }
-    void SolveRight(vector<int>& height, vector<int>& right){
-        stack<int> st;
-
-        for(int i=height.size()-1;i>=0;i--){
-            if(st.empty()){
-                st.push(i);
-                right.push_back(i);
-            }
-            else{
-                while(!st.empty() && height[st.top()] < height[i]){
-                    st.pop();
-                }
-                if(!st.empty()) right.push_back(st.top());
-                else right.push_back(height.size()-1);
-                st.push(i);
-            }
-            
-        }
-        reverse(right.begin(),right.end());
-        
-    }
     int maxArea(vector<int>& height) {
-       int ans =0;
-       int i=0,j=height.size()-1;
-       while(i<j){
-            ans = max(ans,min(height[i],height[j])*(j-i));
-            if(height[i]<height[j]) i++;
-            else j--;
-       }
-       return ans;
+        int left = 0;
+        int right = height.size()-1;
+        int ans = 0;
+        while(left<right){
+            int area = (right-left)*(min(height[left],height[right]));
+            if(height[left]>height[right]) right--;
+            else left++;
+            ans = max(ans,area);
+        }
+        return ans;
     }
 };
