90529a2e21bec448a5fcfa591322e6aeb8ee4283
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-16 13:59:30 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.empty()) return true;
-        transform(s.begin(),s.end(),s.begin(), ::tolower);
-        // after converting it to lower we can use stack 
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')) st.push(s[i]);
-        }
-        for(int i=0;i<s.size();i++){
-            if((s[i]>='a' && s[i]<='z')  || (s[i]>='0' && s[i]<='9')){
-            if(st.top()!= s[i]) return false;
-            cout<<st.top()<<endl;
-            st.pop();
-        }
+  
+        int left = 0;
+        int right = s.size()-1;
+
+        while(left<right){
+            while(left<right && !isalnum(s[left])) left++;
+            while(left<right && !isalnum(s[right])) right--;
+            if(tolower(s[left])!=tolower(s[right])) return false;
+            left++;right--;
         }
         return true;
     }
