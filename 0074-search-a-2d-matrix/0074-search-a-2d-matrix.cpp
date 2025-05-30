83a80e7b1ade884c6fca8c9f0de215ae40ba2a41
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-30 13:29:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // first we have to search for row;
-        int low=0,high = matrix.size()-1;
-        int row = 0;
-        while(low<=high){
-            int mid = (low+high)/2;
-            //row = mid;
-            if(matrix[mid][matrix[0].size()-1]==target) return true;
-            if(matrix[mid][matrix[0].size()-1]<target) low = mid+1;
-            else{
-            row = mid;
-            high=mid-1;
-            
-            } 
-        }
-
-        // we will take high as the row
         
-       
+        // first we needd to find out the row to whcioh that target might belong
+        int left = 0,right = matrix.size()-1;
+        int row = -1;
+        int n = matrix[0].size();
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(matrix[mid][n-1] == target) return true;
+           
+            if(matrix[mid][n-1] > target){
+                row = mid;
+                right = mid-1;
+            }
+            else{
+                
+                left = mid+1;
+            }
+        }
         cout<<row<<endl;
-        low =0; high = matrix[0].size()-1;
-        while(low<=high){
-            int mid = (low+high)/2;
+        // nOw we got the than we will search in that row
+        left = 0;right = matrix[0].size()-1;
+        while(left<=right){
+            int mid = (left+right)/2;
             if(matrix[row][mid]==target) return true;
-            if(matrix[row][mid]<target) low=mid+1;
-            else high=mid-1;
+
+            if(matrix[row][mid]>target) right = mid-1;
+            else left = mid+1;
         }
 
         return false;
-
     }
 };
