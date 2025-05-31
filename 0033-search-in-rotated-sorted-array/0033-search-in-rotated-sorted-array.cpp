b5d091c0b2a7aea419835414104c825ecc56a8d7
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-05-31 10:26:40 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,24 +4,26 @@ public:
         int low=0,high = nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
-            cout<<low<<" "<<mid<<" "<<high<<endl;
             if(nums[mid]==target) return mid;
 
             // if first half is sorted
             if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
                     high = mid-1;
                 }
-                else
-                    low=mid+1;
+                else{
+                    low = mid+1;
+                }
             }
             else{
-                if(nums[mid]<=target && nums[high]>=target){
-                    low=mid+1;
+                if(nums[mid]<target && nums[high]>=target){
+                    low = mid+1;
                 }
-                else
-                    high=mid-1;
+                else{
+                    high = mid-1;
+                }
             }
+
         }
         return -1;
     }
