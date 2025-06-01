00976a7648de81a69881e4c33ed9b68048280ead
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-06-01 10:11:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,12 +10,24 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         // first method is to store in the set.
-        unordered_set<ListNode*> st;
-        ListNode* temp=head;
-        while(temp != NULL){
-            if(st.find(temp) != st.end()) return true;
-            st.insert(temp);
-            temp = temp->next;
+        // unordered_set<ListNode*> st;
+        // ListNode* temp=head;
+        // while(temp != NULL){
+        //     if(st.find(temp) != st.end()) return true;
+        //     st.insert(temp);
+        //     temp = temp->next;
+        // }
+        // return false;
+
+        // nOw heir and tortoise method
+        if(head == NULL) return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL){
+            slow = slow->next;
+            fast =fast->next->next;
+            if(slow == fast) return true;
         }
         return false;
     }
