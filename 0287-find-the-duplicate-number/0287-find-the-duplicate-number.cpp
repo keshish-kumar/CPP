95a458ba8c0c4873f008564b185df03e6f15e513
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-06-03 08:08:55 +0530

    Time: 8 ms (47.94%), Space: 65.1 MB (68.97%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -5,18 +5,19 @@ public:
         int fast = nums[0];
         slow = nums[slow];
         fast = nums[nums[fast]];
-        // find the cycle
-        while(slow != fast){
-            slow = nums[slow];
+        // find if cycle is present or not
+        while(slow!=fast){
+            slow=nums[slow];
             fast = nums[nums[fast]];
         }
 
-        // find the entrance to that cycle
+        // find the entrance to the ycle
         slow = nums[0];
         while(slow!=fast){
             slow = nums[slow];
             fast = nums[fast];
         }
+
         return slow;
     }
 };
