61604f2304d9e9dcfbb91a9dbd333fa7a6382e47
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-06-16 11:07:56 +0530

    Time: 100 ms (37.5%), Space: 182 MB (42.98%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,32 +1,46 @@
 class LRUCache {
 public:
-    list<int> dll;
     unordered_map<int,pair<list<int>::iterator,int>> mp;
+    list<int> dll; // stores the key 
     int n;
     LRUCache(int capacity) {
-         n = capacity;
+        n = capacity;
     }
     
     int get(int key) {
-        if(mp.find(key)!=mp.end()){
+        // if key is already present in map/dll
+        if(mp.find(key) != mp.end()){
             dll.erase(mp[key].first);
             dll.push_front(key);
             mp[key] = {dll.begin(),mp[key].second};
             return mp[key].second;
         }
+
         return -1;
+
     }
     
+    // always remebr recently added or used will be at the begining 
+    // least recently used will be at the last
     void put(int key, int value) {
+        // if key is already present in list;
+        // fidn the address and than dlete that node form that address
         if(mp.find(key)!=mp.end()){
-            dll.erase(mp[key].first);
+            dll.erase(mp[key].first);// delete that node
             dll.push_front(key);
             mp[key] = {dll.begin(),value};
-            return;
         }
-        if(dll.size()>=n){mp.erase(dll.back()); dll.pop_back();}
-        dll.push_front(key);
-        mp[key]={dll.begin(),value};
+        else{
+            dll.push_front(key);
+            mp[key] = {dll.begin(),value};
+        }
+
+        if(dll.size()>n){
+
+            int k  = dll.back();
+            dll.pop_back();
+            mp.erase(k);
+        }
     }
 };
 
