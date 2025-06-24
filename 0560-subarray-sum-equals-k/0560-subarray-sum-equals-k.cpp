b02acab492b8b852f3771ca1b06c78621ea4c04d
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-06-24 15:01:22 +0530

    Time: 73 ms (34.46%), Space: 47.8 MB (31.39%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        // int ans =0;
-        // int sum =0;
-        // int j=0;
-        // for(int i=0;i<nums.size();i++){
-        //     sum+=nums[i];
-        //      while(j<i && sum>k){
-        //         sum=sum-nums[j];
-        //         j++;
-        //     }
-        //     if(sum==k){
-        //         ans++;
-        //     }
-        // }
-        // return ans; // this will work only for positive numbers.'
-
-        map<int,int> mp;
-       int sum=0;
+    int subarraySum(vector<int>& nums, int k) {
+       map<int,int> mp;
+       int sum =0;
        int ans =0;
-    mp[0]=1;
-    for(int i=0;i<arr.size();i++){
-        sum += arr[i];
+       mp[0]=1;
+       for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(mp.find(sum-k)!=mp.end()){
+                ans += mp[sum-k];
+            }
 
-        if(mp.find(sum-k)!=mp.end()){
-            ans+=mp[sum-k];
-        }
-       
-        mp[sum]++;
-    }
-    return ans;
+            mp[sum]++;
+
+       }
+
+       return ans;
+
     }
 };
