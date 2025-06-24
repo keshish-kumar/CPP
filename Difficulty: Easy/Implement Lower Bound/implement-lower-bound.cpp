5ece4bcde82c80745bbc76b793cc3792bae69fac
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-06-24 17:36:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+class Solution {
+  public:
+    int lowerBound(vector<int>& nums, int target) {
+        // code here
+        int low = 0,high = nums.size()-1;
+        int ans = nums.size();
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]>=target){ ans = mid;high = mid-1;}
+            else low = mid+1;
+        }
+        return ans;
+    }
+};
