657c58109a66b204e0b333ad2924aaf35cfb68c4
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-03 17:12:38 +0530

    Time: 0 ms (100%), Space: 16.1 MB (51.32%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int buy,int i,vector<vector<int>>& dp){
+    int solve(vector<int>& prices, int i, bool buy, vector<vector<int>>& dp){
         if(i>=prices.size()) return 0;
         if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy==0){
-            return dp[i][buy] = max(-prices[i]+solve(prices,!buy,i+1,dp),solve(prices,buy,i+1,dp));
+        if(!buy){
+            return dp[i][buy] = max(-prices[i]+solve(prices,i+1,!buy,dp) , solve(prices,i+1,buy,dp));
         }
         else{
-            return dp[i][buy] = max(prices[i]+solve(prices,!buy,i+2,dp),solve(prices,buy,i+1,dp));
+            return dp[i][buy] = max(prices[i]+ solve(prices,i+2,!buy,dp),solve(prices,i+1,buy,dp));
         }
+
     }
     int maxProfit(vector<int>& prices) {
-        // there is cool down periojd which need to be considered
-        int buy=0;
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return solve(prices,buy,0,dp);
+        bool buy = false; 
+        int val = 0;
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,-1));
+        return solve(prices,0,buy,dp);
     }
 };
