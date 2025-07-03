cf9fefc706376b01ae896c80d984e8884b08377f
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-03 16:20:36 +0530

    Time: 58 ms (90.36%), Space: 111.1 MB (21.55%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,40 +2,20 @@ class Solution {
 public:
     bool solve(vector<int>& nums, int target, int i,vector<vector<int>>& dp){
         if(target == 0) return true;
-        if(target<0 || i>=nums.size()) return false;
+        if(i>=nums.size()||target<0) return false;
+
         if(dp[i][target]!=-1) return dp[i][target];
-        dp[i][target] = solve(nums,target-nums[i],i+1,dp) || solve(nums,target,i+1,dp);
-        return dp[i][target];
+
+        return dp[i][target] = solve(nums,target-nums[i],i+1,dp)||solve(nums,target,i+1,dp);
+
     }
     bool canPartition(vector<int>& nums) {
-        //. equal partititon can onky be performed when sum is even number odd number cannot be partriitoned equally
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum%2!=0) return false;
 
         int target = sum/2;
-       // vector<vector<int> > dp(nums.size(),vector<int>(target+1,-1));
-       // return solve(nums,target,0,dp);
-        
-        // lets try using tabulation approach
-        // if there are no elemlents than always false for particluao target
-        // if target == 0 than always true
-        vector<bool> dp(target+1,false);
-        dp[0] = true;
-        for(int i=1;i<=nums.size();i++){
-            vector<bool> temp(target+1,false);
-            temp[0]=true;
-            for(int j=1;j<=target;j++){
+        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
 
-                    bool pick = dp[j];
-                    bool notpick = false;
-                    if(nums[i-1]<=j){
-                        notpick = dp[j-nums[i-1]];
-                    }
-                    temp[j] = pick||notpick;
-                
-            }
-            dp = temp;
-        }
-        return dp[target];
+        return solve(nums,target,0,dp);
     }
 };
