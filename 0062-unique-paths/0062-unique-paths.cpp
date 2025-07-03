b61167559ffe8a501d0476e847be1ee68878fb81
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-03 16:29:40 +0530

    Time: 2 ms (19.06%), Space: 9.4 MB (33.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,8 +14,27 @@ public:
         return dp[i][j];
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m,n,0,0,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        // return solve(m,n,0,0,dp);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int l = 0;
+                    if(i-1 >= 0){
+                        l = dp[i-1][j];
+                    }
+                    int r = 0;
+                    if(j-1>=0){
+                        r = dp[i][j-1];
+                    }
+                    dp[i][j] = l+r;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
         
     }
 };
