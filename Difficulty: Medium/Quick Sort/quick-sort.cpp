e4c506aa0df855a7ed76ba3e0a3159bfff08c507
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-05 01:39:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -1,25 +1,13 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Function to print an array */
-void printArray(const vector<int>& arr) {
-    for (int num : arr)
-        printf("%d ", num);
-    printf("\n");
-}
-
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to sort an array using quick sort algorithm.
     void quickSort(vector<int>& arr, int low, int high) {
         // code here
         if(low<high){
-            int p = partition(arr,low,high);
-            quickSort(arr,low,p-1);
-            quickSort(arr,p+1,high);
+        int pivot = partition(arr,low,high);
+        
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
         }
     }
 
@@ -28,53 +16,21 @@ class Solution {
     // its correct position in sorted array, and places all smaller elements
     // to left of pivot and all greater elements to right of pivot.
     int partition(vector<int>& arr, int low, int high) {
-        // code here
-        int pivot = low;
-        int p=low;
-        int q = high;
-        while(p<q){
-            
-        while(arr[p]<=arr[pivot])
-            p++;
-        while(arr[q]>arr[pivot]){
-            q--;
-        }
-        if(p<q){
-            swap(arr[p],arr[q]);
-        }
-            
-        }
-     
-        swap(arr[pivot],arr[q]);
         
-        return q;
+        // code here
+        int pivot = high;
+        int pi = low; 
+        int j=low;
+        while(j<high){
+            if(arr[j]<=arr[pivot]){
+                swap(arr[pi],arr[j]);
+                pi++;
+            }
+            j++;
+        }
+        
+        swap(arr[pi],arr[pivot]);
+        return pi;
+        
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int T;
-    scanf("%d", &T);
-    getchar(); // to consume newline after T
-
-    while (T--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        ob.quickSort(arr, 0, arr.size() - 1);
-        printArray(arr);
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
