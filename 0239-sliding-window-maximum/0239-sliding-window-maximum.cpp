a4e093e2add682e211e40825b49197849224a7cf
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-04 12:35:34 +0530

    Time: 27 ms (46.76%), Space: 139.1 MB (60.2%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // stores indices of useful elements in a decreasing order
         vector<int> ans;
-
+        deque<int> dq;
         int i=0,j=0;
-        while(j<nums.size()){
-            while(!dq.empty() && dq.back()<nums[j]){
+        while(j<nums.size()){                        
+            while(!dq.empty() && nums[j]>nums[dq.back()]){
                 dq.pop_back();
             }
-            dq.push_back(nums[j]);
+            dq.push_back(j);
             if((j-i+1)==k){
-                ans.push_back(dq.front());
-                if(nums[i]==dq.front()) dq.pop_front();
+                ans.push_back(nums[dq.front()]);
+                if(dq.front() == i) dq.pop_front();
                 i++;
             }
             j++;
         }
-        
         return ans;
     }
 };
