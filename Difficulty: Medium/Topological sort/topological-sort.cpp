d3452732606283a2b375849f6076f43b1f14da3c
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-07 12:40:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,88 +1,34 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    stack<int> st ; // this will store the alst node of stack.
-	    vector<int> indegree(V,0);
-	    for(int i=0;i<V;i++){
-	        for(auto it:adj[i]){
-	            indegree[it]++;
-	        }
-	    }
-	    
-	    // Now we will apply dfs to this and check wether its gibing all the vertices or not
-	    queue<int> q;
-	    for(int i=0;i<V;i++){
-	        if(indegree[i]==0) q.push(i);
-	    }
-	    vector<int> ans;
-	    while(!q.empty()){
-	        
-	        ans.push_back(q.front());
-	        for(auto it:adj[q.front()]){
-	            indegree[it]--;
-	            if(indegree[it]==0) q.push(it);
-	        }
-	        q.pop();
-	    }
-	    return ans;
-	}
-};
-
-//{ Driver Code Starts.
-
-/*  Function to check if elements returned by user
-*   contains the elements in topological sorted form
-*   V: number of vertices
-*   *res: array containing elements in topological sorted form
-*   adj[]: graph input
-*/
-int check(int V, vector <int> &res, vector<int> adj[]) {
-    
-    if(V!=res.size())
-    return 0;
-    
-    vector<int> map(V, -1);
-    for (int i = 0; i < V; i++) {
-        map[res[i]] = i;
-    }
-    for (int i = 0; i < V; i++) {
-        for (int v : adj[i]) {
-            if (map[i] > map[v]) return 0;
-        }
-    }
-    return 1;
-}
-
-int main() {
-    int T;
-    cin >> T;
-    while (T--) {
-        int N, E;
-        cin >> E >> N;
-        int u, v;
-
-        vector<int> adj[N];
-
-        for (int i = 0; i < E; i++) {
-            cin >> u >> v;
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        vector<int> indegree(V,0);
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
             adj[u].push_back(v);
+            indegree[v]++;
         }
         
-        Solution obj;
-        vector <int> res = obj.topoSort(N, adj);
-
-        cout << check(N, res, adj) << endl;
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                int i = q.front();
+                q.pop();
+                ans.push_back(i);
+                for(auto it:adj[i]){
+                    indegree[it]--;
+                    if(indegree[it]==0) q.push(it);
+                }
+            }
+        }
+        return ans;
+        
     }
-    
-    return 0;
-}
-// } Driver Code Ends
+};
