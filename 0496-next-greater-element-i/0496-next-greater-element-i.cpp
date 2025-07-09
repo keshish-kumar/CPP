4053161ae9b474bd5c936be8db70e2a31f7fcd11
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-09 14:31:04 +0530

    Time: 0 ms (100%), Space: 12.7 MB (73.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // Here we will use monotonic stack
+        
         stack<int> st;
-        unordered_map<int, int> mp;
-        for(int i=nums2.size()-1;i>=0;i--){
-            if(st.empty()){
-                mp[nums2[i]] = -1;
-                st.push(nums2[i]);
+        vector<int> ans(nums2.size(),-1);
+         for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty() && st.top()<nums2[i]){
+                st.pop();
             }
-            else{
-                while(!st.empty() && st.top()<nums2[i]){
-                    st.pop();
-                }
-                if(!st.empty()){mp[nums2[i]] = st.top(); }
-                else mp[nums2[i]] = -1;
-                st.push(nums2[i]);
-            }
+            if(!st.empty()) ans[i] = st.top();
+            st.push(nums2[i]);
+         }
+
+         unordered_map<int,int> mp;
+        for(int i=0;i<nums2.size();i++){
+            mp[nums2[i]] = ans[i];
         }
 
-        // Now that in map we have stored element and their next graeter we will find the answer in nums1
-
-        vector<int> ans;
+       ans = {};
         for(int i=0;i<nums1.size();i++){
             ans.push_back(mp[nums1[i]]);
         }
