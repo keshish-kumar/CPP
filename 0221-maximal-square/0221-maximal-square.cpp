afe3f92ff8fa76e730b00720d57be3f93cf0fde3
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-09 17:40:07 +0530

    Time: 15 ms (30.39%), Space: 31.1 MB (34.17%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        
-        vector<vector<int>> temp(matrix.size(),vector<int>(matrix[0].size(),0));
+        vector<vector<int>> temp(matrix.size(),vector<int>(matrix[0].size()));
+        int ans = 0;
         for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
+            for(int j=0;j<matrix[0].size();j++){
                 temp[i][j] = matrix[i][j]-'0';
+                //ans = max(ans,temp[i][j]);
             }
         }
-        int maxi = 0;
+        
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[i].size();j++){
-                if((i-1)>=0 && (j-1)>=0 && temp[i][j]==1){
-                    temp[i][j] += min(temp[i-1][j],min(temp[i][j-1],temp[i-1][j-1]));
+                if((i-1>=0) && (j-1)>=0 && temp[i][j] ==1) {
+                    temp[i][j] = temp[i][j]+ min({temp[i-1][j],temp[i][j-1],temp[i-1][j-1]});
                 }
-                maxi = max(maxi,temp[i][j]);
-                //cout<<temp[i][j]<<" ";
+                ans = max(ans,temp[i][j]);
             }
-            //cout<<endl;
         }
-        return maxi*maxi;
+        return ans*ans;
     }
 };
