fab69888925250a9daa2d854cf3876f89964937b
keshish kumar <61411118+keshish-kumar@users.noreply.github.com>
2025-07-29 11:23:56 +0530

    Time: 0 ms (100%), Space: 21.6 MB (74.17%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    // static bool compr(vector<int>& a, vector<int>& b){
-    //     return a[0]<b[0];
-    // }
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        // first I have to check at what interval I need to insert this element 
+        vector<vector<int>> ans;
+        int i=0;
+        while(i<intervals.size() && intervals[i][1]<newInterval[0]){
+            ans.push_back(intervals[i]);
+            i++;
+        }
 
-            vector<vector<int>> ans;
+        // Now we will inert the interval
+        while(i<intervals.size() && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = min(intervals[i][0],newInterval[0]);
+            newInterval[1] = max(intervals[i][1],newInterval[1]);
+            i++;
+        }
 
-            // first we have to which all elenments comes before the interval
+        ans.push_back(newInterval);
 
-            int i=0;
-            while(i<intervals.size() && intervals[i][1]<newInterval[0]){
-                ans.push_back(intervals[i]);
-                i++;
-            }
+        while(i<intervals.size()){
+            ans.push_back(intervals[i]);
+            i++;
+        }
 
-            // after fidning out where it is object
-            
-            while(i<intervals.size() && intervals[i][0]<=newInterval[1]){
-                newInterval[0] = min(newInterval[0],intervals[i][0]);
-                newInterval[1] = max(newInterval[1],intervals[i][1]);
-                i++;
-            }
-
-            ans.push_back(newInterval);
-
-            // Now push the rest of the lement in it
-
-
-            while(i<intervals.size()){
-                ans.push_back(intervals[i]);
-                i++;
-            }
         return ans;
     }
 };
